4999d3a86f9de0c82821fe8b8d20571160cce4a8
= <liwenfeisix@163.com>
2017-03-02 13:03:00 +0800

    add cacheStatusReport

--- hadoop-ssm-project/src/main/java/org/apache/hadoop/ssm/CacheStatus.java
+++ hadoop-ssm-project/src/main/java/org/apache/hadoop/ssm/CacheStatus.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.ssm;
 
 import java.util.List;
@@ -9,10 +26,10 @@ import java.util.Map;
 public class CacheStatus {
   // the key named cachePoolName
   private Map<String, List<cacheFileInfo>> cacheStatusMap;
-  private int cacheCapacity;
-  private int cacheUsed;
-  private int cacheRemaining;
-  private int cacheUsedPercentage;
+  private long cacheCapacity;
+  private long cacheUsed;
+  private long cacheRemaining;
+  private float cacheUsedPercentage;
 
   public CacheStatus() {
     cacheCapacity = 0;
@@ -21,7 +38,7 @@ public class CacheStatus {
     cacheUsedPercentage = 0;
   }
 
-  class cacheFileInfo {
+   class cacheFileInfo {
     private String filePath;
     private int repliNum;
 
@@ -47,43 +64,43 @@ public class CacheStatus {
     }
   }
 
-  public int getCacheCapacity() {
-    return cacheCapacity;
-  }
-
-  public void setCacheCapacity(int cacheCapacity) {
-    this.cacheCapacity = cacheCapacity;
-  }
-
-  public int getCacheUsed() {
-    return cacheUsed;
-  }
-
-  public void setCacheUsed(int cacheUsed) {
-    this.cacheUsed = cacheUsed;
-  }
-
-  public int getCacheRemaining() {
-    return cacheRemaining;
-  }
-
-  public void setCacheRemaining(int cacheRemaining) {
-    this.cacheRemaining = cacheRemaining;
-  }
-
-  public int getCacheUsedPercentage() {
-    return cacheUsedPercentage;
-  }
-
-  public void setCacheUsedPercentage(int cacheUsedPercentage) {
-    this.cacheUsedPercentage = cacheUsedPercentage;
-  }
-
   public Map<String, List<cacheFileInfo>> getCacheStatusMap() {
     return cacheStatusMap;
   }
 
   public void setCacheStatusMap(Map<String, List<cacheFileInfo>> cacheStatusMap) {
     this.cacheStatusMap = cacheStatusMap;
+  }
+
+  public long getCacheCapacity() {
+    return cacheCapacity;
+  }
+
+  public void setCacheCapacity(long cacheCapacity) {
+    this.cacheCapacity = cacheCapacity;
+  }
+
+  public long getCacheUsed() {
+    return cacheUsed;
+  }
+
+  public void setCacheUsed(long cacheUsed) {
+    this.cacheUsed = cacheUsed;
+  }
+
+  public long getCacheRemaining() {
+    return cacheRemaining;
+  }
+
+  public void setCacheRemaining(long cacheRemaining) {
+    this.cacheRemaining = cacheRemaining;
+  }
+
+  public float getCacheUsedPercentage() {
+    return cacheUsedPercentage;
+  }
+
+  public void setCacheUsedPercentage(float cacheUsedPercentage) {
+    this.cacheUsedPercentage = cacheUsedPercentage;
   }
 }
