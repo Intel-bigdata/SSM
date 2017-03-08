4e2c6a6bf91c427e78613f6c68894764ebcb4b16
You Yanping <yanping.you@intel.com>
2017-03-08 16:00:14 +0800

    every new run button can run different command

--- hadoop-ssm-project/src/main/webapps/command/runcommand.js
+++ hadoop-ssm-project/src/main/webapps/command/runcommand.js
@@ -15,86 +15,42 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-(function() {
-  "use strict";
-    var i = 0;
-    $('#btn-run-cmd').click(function () {
-
-    var url = '/ssm/v1?op=RUNCOMMAND&cmd=' + $('#cmd').val()
-
+"use strict";
+function run(var1) {
+    var number = var1.substring(var1.length-1);
+    alert(number);
+    var url = '/ssm/v1?op=RUNCOMMAND&cmd=' + $('#cmd'+number).val();
     $.ajax({
         type: 'PUT',
         url: url
     }).then(function(data) {
-        $('#stdout').html('');
-        $('#stderr').html('');
-        for (var i=0;i<data.stdout.length;i++) {
-            $('#stdout').append(data.stdout[i]+'<br>');
+        $('#stdout'+number).html('');
+        $('#stderr'+number).html('');
+        for (var j=0;j<data.stdout.length;j++) {
+            $('#stdout'+number).append(data.stdout[j]+'<br>');
         }
-
-        for (var i=0;i<data.stderr.length;i++) {
-            $('#stderr').append(data.stderr[i]+'<br>');
+        for (var j=0;j<data.stderr.length;j++) {
+            $('#stderr'+number).append(data.stderr[j]+'<br>');
         }
     });
+};
 
-    });
+var i = 0;
+function newDiv() {
+    i = i+1;
+    var $div = $('<form onsubmit="return false;">'
+     +'<div class="input-group">'
+         +'<input type="text" class="form-control" id="cmd'+i+'"/>'
+         +'<span class="input-group-btn">'
+         +'<button class="btn btn-default" type="button" onclick="run(this.id)" id="btn-run-cmd'+i+'">Run!</button>'
+         +'</span>'
+     +'</div>'
+     +'<div>'
+         +'<p id="stdout'+i+'">stdout is </p>'
+         +'<p id="stderr'+i+'">stderr is </p>'
+     +'</div>'
+    +'</form>');
+    $("#div").append($div);
 
+};
 
-//     $('[id^=btn-run-cmd]').click(function () {
-//
-//          var url = '/ssm/v1?op=RUNCOMMAND&cmd=' + $('[id^=btn-run-cmd]').parent().prev().val();
-//          alert();
-//
-//          $.ajax({
-//              type: 'PUT',
-//              url: url
-//          }).then(function(data) {
-//              $('[id^=btn-run-cmd]').parent().parent().prev().children(":first").html('');
-//              $('[id^=btn-run-cmd]').parent().parent().prev().children(":last").html('');
-//              for (var i=0;i<data.stdout.length;i++) {
-//                  $('[id^=btn-run-cmd]').parent().parent().prev().children(":first").append(data.stdout[i]+'<br>');
-//              }
-//
-//              for (var i=0;i<data.stderr.length;i++) {
-//                  $('[id^=btn-run-cmd]').parent().parent().prev().children(":last").append(data.stderr[i]+'<br>');
-//              }
-//          });
-
-//     });
-
-
-//    $('#btn-show-cache').click(function () {
-//
-//    var url = '/ssm/v1?op=SHOWCACHE'
-//
-//    $.ajax({
-//        type: 'GET',
-//        url: url
-//    }).then(function(data) {
-//       $('.cachestatus').text(data.cacheUsedPercentage);
-//    });
-//
-//    });
-
-
-     $('#btn-new').click(function () {
-                 i = i+1;
-                 var $div = $('<form onsubmit="return false;">'
-                     +'<div class="input-group">'
-                         +'<input type="text" class="form-control" id="cmd'+i+'"/>'
-                         +'<span class="input-group-btn">'
-                         +'<button class="btn btn-default" type="button" id="btn-run-cmd'+i+'">Run!</button>'
-                         +'</span>'
-                     +'</div>'
-                     +'<div>'
-                         +'<p id="stdout'+i+'">stdout is </p>'
-                         +'<p id="stderr'+i+'">stderr is </p>'
-                     +'</div>'
-                 +'</form>');
-                 $("#div").append($div);
-
-     });
-
-
-
-})();
