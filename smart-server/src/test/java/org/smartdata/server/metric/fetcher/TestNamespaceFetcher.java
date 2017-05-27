a0ca9fd1b47cb62889d38702896e3da9436de5ca
huafengw <fvunicorn@gmail.com>
2017-05-27 13:46:35 +0800

    minor

--- smart-server/src/test/java/org/smartdata/server/metric/fetcher/TestNamespaceFetcher.java
+++ smart-server/src/test/java/org/smartdata/server/metric/fetcher/TestNamespaceFetcher.java
@@ -36,7 +36,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.mock;
