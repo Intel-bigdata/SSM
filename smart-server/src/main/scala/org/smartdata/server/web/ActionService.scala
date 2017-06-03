edd642463b4564e4003b26aa73dbdb7b06492265
huafengw <fvunicorn@gmail.com>
2017-06-03 10:59:10 +0800

    load action type list from backend

--- smart-server/src/main/scala/org/smartdata/server/web/ActionService.scala
+++ smart-server/src/main/scala/org/smartdata/server/web/ActionService.scala
@@ -18,13 +18,14 @@
 package org.smartdata.server.web
 
 import java.util
-import scala.collection.JavaConverters._
 
+import scala.collection.JavaConverters._
 import akka.http.scaladsl.server.Directives.{complete, path, pathPrefix, _}
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.ParameterDirectives.ParamMagnet
 import akka.stream.Materializer
 import com.google.gson.Gson
+import org.smartdata.actions.ActionDescriptor
 import org.smartdata.common.actions.ActionInfo
 import org.smartdata.server.SmartServer
 
@@ -46,12 +47,19 @@ class ActionService(ssmServer: SmartServer) extends BasicService {
     .setLog("objc[41272]: Class JavaLaunchHelper is implemented in both /Library/Java/JavaVirtualMachines/jdk1.8.0_111.jdk/Contents/Home/bin/java (0x1075fd4c0) and /Library/Java/JavaVirtualMachines/jdk1.8.0_111.jdk/Contents/Home/jre/lib/libinstrument.dylib (0x1076d94e0). One of the two will be used. Which one is undefined.")
   actions.add(builder.build())
 
+  private val actionTypes: util.Collection[ActionDescriptor] = new util.ArrayList[ActionDescriptor]()
+  actionTypes.add(new ActionDescriptor("ls", "List files", "Comment", "Usage"))
+  actionTypes.add(new ActionDescriptor("write", "Write files", "Comment", "Usage"))
+
   override protected def doRoute(implicit mat: Materializer): Route =
     pathPrefix("actions" / IntNumber) { actionId =>
       path("detail") {
         complete(gson.toJson(actions.asScala.find(_.getActionId == actionId).get))
       }
     } ~
+      path("actiontypes") {
+        complete(gson.toJson(actionTypes))
+      } ~
       path("actionlist") {
         complete(gson.toJson(actions))
       } ~
