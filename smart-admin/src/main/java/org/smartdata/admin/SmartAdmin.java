18481e60f7e287f05b4f7c53bfc1df1ee05c0c2c
littlezhou <wei.zhou@intel.com>
2017-06-03 10:35:21 +0800

    Solve #300, Implement RPC calls for execute actions directly. By Yao Chen

--- smart-admin/src/main/java/org/smartdata/admin/SmartAdmin.java
+++ smart-admin/src/main/java/org/smartdata/admin/SmartAdmin.java
@@ -22,6 +22,7 @@ import org.apache.hadoop.ipc.ProtobufRpcEngine;
 import org.apache.hadoop.ipc.RPC;
 import org.smartdata.admin.protocolPB.SmartAdminProtocolAdminSideTranslatorPB;
 import org.smartdata.common.CommandState;
+import org.smartdata.common.actions.ActionDescriptor;
 import org.smartdata.common.actions.ActionInfo;
 import org.smartdata.conf.SmartConfKeys;
 import org.smartdata.common.SmartServiceState;
@@ -158,5 +159,17 @@ public class SmartAdmin implements java.io.Closeable, SmartAdminProtocol {
   public List<ActionInfo> listActionInfoOfLastActions(int maxNumActions) throws IOException {
     checkOpen();
     return ssm.listActionInfoOfLastActions(maxNumActions);
+  }
+
+  @Override
+  public long submitCommand(String cmd) throws IOException {
+    checkOpen();
+    return ssm.submitCommand(cmd);
+  }
+
+  @Override
+  public List<ActionDescriptor> listActionsSupported() throws IOException {
+    checkOpen();
+    return ssm.listActionsSupported();
   }
 }
