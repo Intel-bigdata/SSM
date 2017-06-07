336ebc0a7721dbc8d526acb7aadf3136007ccf45
huafengw <fvunicorn@gmail.com>
2017-06-07 18:13:29 +0800

    polish dashboard

--- smart-server/src/main/dashboard/views/actions/actions.js
+++ smart-server/src/main/dashboard/views/actions/actions.js
@@ -63,14 +63,16 @@ angular.module('dashboard')
         cols: [
           // group 1/3 (4-col)
           $stb.indicator().key('state').canSort('state.condition+"_"+createTime').styleClass('td-no-padding').done(),
-          $stb.text('ID').key('id').canSort().done(),
+          $stb.text('ID').key('id').canSort().sortDefaultDescent().done(),
           $stb.text('Command ID').key('cid').canSort().done(),
           $stb.text('Name').key('actionName').canSort().done(),
             // $stb.link('Name').key('name').canSort('name.text').styleClass('col-md-1').done(),
           // group 2/3 (5-col)
-          $stb.datetime('Create Time').key('createTime').canSort().sortDefaultDescent().done(),
-          $stb.datetime('Finish Time').key('finishTime').canSort().sortDefaultDescent().done(),
-          // $stb.datetime('Start Time').key('startTime').canSort().styleClass('col-md-1 hidden-sm hidden-xs').done(),
+          $stb.datetime('Create Time').key('createTime').canSort().done(),
+          $stb.datetime('Finish Time').key('finishTime').canSort().done(),
+          $stb.duration("Running Time").key('runningTime').canSort().done(),
+
+            // $stb.datetime('Start Time').key('startTime').canSort().styleClass('col-md-1 hidden-sm hidden-xs').done(),
           // $stb.datetime('Stop Time').key('stopTime').canSort().styleClass('col-md-1 hidden-sm hidden-xs').done(),
           // $stb.text('User').key('user').canSort().styleClass('col-md-2').done(),
           // group 3/3 (4-col)
@@ -92,10 +94,11 @@ angular.module('dashboard')
               //user: rule.user,
               actionName: action.actionName,
               createTime: action.createTime,
-              finishTime: action.finished ? action.finishTime : "",
+              finishTime: action.finished ? action.finishTime : "-",
+              runningTime: action.uptime,
               // startTime: rule.startTime,
               // stopTime: rule.finishTime || '-',
-              succeed: action.successful,
+              succeed: action.finished ? action.successful : "-",
               view: {
                 href: action.pageUrl,
                 text: 'Show Result',
