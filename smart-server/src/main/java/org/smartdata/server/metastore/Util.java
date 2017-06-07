a4a13aef9d1f1e70c0086b7b03e2624e69a8b493
littlezhou <wei.zhou@intel.com>
2017-06-07 20:00:19 +0800

    Solve #370, Fix 'sid' value error in Table 'files'

--- smart-server/src/main/java/org/smartdata/server/metastore/Util.java
+++ smart-server/src/main/java/org/smartdata/server/metastore/Util.java
@@ -135,6 +135,7 @@ public class Util {
             "  `policy_name` varchar(64) DEFAULT NULL\n" +
             ") ;",
 
+        "INSERT INTO `storage_policy` VALUES ('0', 'HOT');" ,
         "INSERT INTO `storage_policy` VALUES ('2', 'COLD');" ,
         "INSERT INTO `storage_policy` VALUES ('5', 'WARM');" ,
         "INSERT INTO `storage_policy` VALUES ('7', 'HOT');" ,
