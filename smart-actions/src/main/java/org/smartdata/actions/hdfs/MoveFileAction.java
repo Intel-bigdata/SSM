417ab68953fa846f97b024f0b9866a3f8ecdd120
littlezhou <wei.zhou@intel.com>
2017-06-08 11:21:00 +0800

    Fix #377, File storage policy not updated in Table 'files' after been moved

--- smart-actions/src/main/java/org/smartdata/actions/hdfs/MoveFileAction.java
+++ smart-actions/src/main/java/org/smartdata/actions/hdfs/MoveFileAction.java
@@ -74,6 +74,7 @@ public class MoveFileAction extends HdfsAction {
 
     try {
       moveRunner.move(fileName);
+      actionStatus.setSuccessful(true);
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
