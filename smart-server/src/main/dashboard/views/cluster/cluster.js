379320d560fe6565b364117d509c9ab115a9c908
yanpingyou <yanping.you@intel.com>
2017-06-09 16:56:58 +0800

    paging cluster into two navigation

--- smart-server/src/main/dashboard/views/cluster/cluster.js
+++ smart-server/src/main/dashboard/views/cluster/cluster.js
@@ -18,15 +18,23 @@
  */
 angular.module('dashboard')
 
-    .config(['$stateProvider',
-        function ($stateProvider) {
+    .config(['$stateProvider','$urlRouterProvider',
+        function ($stateProvider, $urlRouterProvider) {
             'use strict';
+
+            $urlRouterProvider
+                .when('/cluster/','/cluster/hottestFiles')
+                .when('/cluster', '/cluster/hottestFiles');
 
             $stateProvider
                 .state('cluster', {
                     url: '/cluster',
-                    templateUrl: 'views/cluster/cluster.html',
-                    controller: 'ClusterCtrl',
+                    templateUrl: 'views/cluster/cluster.html'
+                })
+                .state('cluster.hottestFiles',{
+                    url:'/hottestFiles',
+                    templateUrl: 'views/cluster/cluster-hottestFiles.html',
+                    controller: 'ClusterCtrl1',
                     resolve: {
                         cached0: ['models', function (models) {
                             return models.$get.cachedfiles();
@@ -35,13 +43,63 @@ angular.module('dashboard')
                             return models.$get.hotFiles();
                         }]
                     }
-                });
+                })
+                .state('cluster.fileInCache',{
+                    url:'/fileInCache',
+                    templateUrl: 'views/cluster/cluster-fileInCache.html',
+                    controller: 'ClusterCtrl2',
+                    resolve: {
+                        cached0: ['models', function (models) {
+                            return models.$get.cachedfiles();
+                        }],
+                        hotfiles0: ['models', function (models) {
+                            return models.$get.hotFiles();
+                        }]
+                    }
+                })
+            ;
         }])
 
     /**
      * This controller is used to obtain app. All nested views will read status from here.
      */
-    .controller('ClusterCtrl', ['$scope', '$state', '$sortableTableBuilder', 'i18n', 'helper', 'models', 'cached0', 'hotfiles0',
+    .controller('ClusterCtrl', ['$state',
+        function ($state) {
+            'use strict';
+
+            $state.go('cluster.hottestFiles');
+        }
+    ])
+    .controller('ClusterCtrl1', ['$scope', '$state', '$sortableTableBuilder', 'i18n', 'helper', 'models', 'cached0', 'hotfiles0',
+        function ($scope, $state, $stb, i18n, helper, models, cached0, hotfiles0) {
+            'use strict';
+
+            $scope.hotfilesTable = {
+                cols: [
+                    $stb.text('File ID').key('id').styleClass('col-md-2').done(),
+                    $stb.text('File Path').key('filePath').styleClass('col-md-2').done(),
+                    $stb.text('Access Count').key('accessCountNum').canSort().sortDefaultDescent().styleClass('col-md-2').done()
+                ],
+                rows: null
+            };
+
+            function updateHotTable(hotFiles) {
+                $scope.hotfilesTable.rows = $stb.$update($scope.hotfilesTable.rows,
+                    _.map(hotFiles, function (file) {
+                        return {
+                            id: file.fid,
+                            filePath: file.path,
+                            accessCountNum: file.accessCount
+                        };
+                    }));
+            }
+
+            updateHotTable(hotfiles0.$data());
+            hotfiles0.$subscribe($scope, function (hotfiles) {
+                updateHotTable(hotfiles);
+            });
+        }])
+    .controller('ClusterCtrl2', ['$scope', '$state', '$sortableTableBuilder', 'i18n', 'helper', 'models', 'cached0', 'hotfiles0',
         function ($scope, $state, $stb, i18n, helper, models, cached0, hotfiles0) {
             'use strict';
 
@@ -69,34 +127,9 @@ angular.module('dashboard')
                     }));
             }
 
-            $scope.hotfilesTable = {
-                cols: [
-                    $stb.text('File ID').key('id').styleClass('col-md-2').done(),
-                    $stb.text('File Path').key('filePath').styleClass('col-md-2').done(),
-                    $stb.text('Access Count').key('accessCountNum').canSort().sortDefaultDescent().styleClass('col-md-2').done()
-                ],
-                rows: null
-            };
-
-            function updateHotTable(hotFiles) {
-                $scope.hotfilesTable.rows = $stb.$update($scope.hotfilesTable.rows,
-                    _.map(hotFiles, function (file) {
-                        return {
-                            id: file.fid,
-                            filePath: file.path,
-                            accessCountNum: file.accessCount
-                        };
-                    }));
-            }
-
             updateTable(cached0.$data());
             cached0.$subscribe($scope, function (cachedfiles) {
                 updateTable(cachedfiles);
-            });
-
-            updateHotTable(hotfiles0.$data());
-            hotfiles0.$subscribe($scope, function (hotfiles) {
-                updateHotTable(hotfiles);
             });
         }])
 ;
