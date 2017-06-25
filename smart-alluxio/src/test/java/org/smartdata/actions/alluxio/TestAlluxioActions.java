ab3b37c890c6ac6e7b829adc5f5d55c359a31770
littlezhou <wei.zhou@intel.com>
2017-06-25 23:57:23 +0800

    print

--- smart-alluxio/src/test/java/org/smartdata/actions/alluxio/TestAlluxioActions.java
+++ smart-alluxio/src/test/java/org/smartdata/actions/alluxio/TestAlluxioActions.java
@@ -22,15 +22,19 @@ public class TestAlluxioActions {
   public void setUp() throws Exception {
     mLocalAlluxioCluster = new LocalAlluxioCluster(2);
     mLocalAlluxioCluster.initConfiguration();
+    System.out.println("setUp ");
     mLocalAlluxioCluster.start();
     fs = FileSystem.Factory.get();
+    System.out.println("setUp ......");
   }
 
   @After
   public void tearDown() throws Exception {
+    System.out.println("tearDown");
     if (mLocalAlluxioCluster != null) {
       mLocalAlluxioCluster.stop();
     }
+    System.out.println("tearDown oooooo");
   }
   
   @Test
@@ -41,17 +45,21 @@ public class TestAlluxioActions {
     FileOutStream fos = fs.createFile(new AlluxioURI("/dir1/file1"), options);
     fos.write(new byte[] {1});
     fos.close();
+    System.out.println("AAAAA");
 
     // check status not persisted
     URIStatus status1 = fs.getStatus(new AlluxioURI("/dir1/file1"));
     assertEquals(status1.getPersistenceState(), "NOT_PERSISTED");
-    
+    System.out.println("BBBBB");
+
     // run persist action
     PersistAction persistAction = new PersistAction();
     Map<String, String> args = new HashMap<>();
     args.put("-path", "/dir1/file1");
     persistAction.init(args);
     persistAction.execute();
+
+    System.out.println("CCCCCC");
     
     // check status persisted
     URIStatus status2 = fs.getStatus(new AlluxioURI("/dir1/file1"));
