ffbfa541f07f90d1ea4c6174d4a859b267c66316
timmyyao <shunyang.yao@intel.com>
2017-06-29 13:15:50 +0800

    Integration test base

--- smart-integration/src/test/java/org/smartdata/integration/TestSystemRestApi.java
+++ smart-integration/src/test/java/org/smartdata/integration/TestSystemRestApi.java
@@ -1,73 +1,15 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.smartdata.integration;
 
-import com.google.gson.Gson;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import org.hamcrest.Matchers;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.smartdata.conf.SmartConf;
-import org.smartdata.conf.SmartConfKeys;
-import org.smartdata.integration.cluster.MiniSmartCluster;
-import org.smartdata.integration.cluster.SmartCluster;
 
 /**
- * Integration test.
+ * Created by root on 6/29/17.
  */
-public class IntegrationTest {
-  private static SmartCluster cluster;
-  private static SmartConf conf;
-  private static IntegrationSmartServer smartServer;
-  private static String httpUri;
-  private static String httpHost;
-  private static int httpPort;
-  private static int zeppelinPort;
-  private static Gson gson = new Gson();
-
-  @BeforeClass
-  public static void setup() throws Exception {
-    // Set up an HDFS cluster
-    cluster = new MiniSmartCluster();
-    cluster.setUp();
-
-    // Start a Smart server
-    conf = cluster.getConf();
-    httpHost = "127.0.0.1";
-    httpPort = 7045;
-    zeppelinPort = 8080;
-    httpUri = httpHost + ":" + httpPort;
-    conf.set(SmartConfKeys.DFS_SSM_HTTP_ADDRESS_KEY, httpUri);
-    smartServer = new IntegrationSmartServer();
-    smartServer.setUp(conf);
-
-    // Initialize RestAssured
-    initRestAssured();
-  }
-
-  private static void initRestAssured() {
-    RestAssured.port = zeppelinPort;
-    //RestAssured.registerParser("text/plain", Parser.JSON);
-  }
-
+public class TestSystemRestApi extends IntegrationTestBase {
   // Just an example
   @Test
   public void testSubmitAction() throws Exception {
@@ -121,11 +63,5 @@ public class IntegrationTest {
         .body("successful[2]", Matchers.equalTo(true))
         .body("successful[3]", Matchers.equalTo(true))
         .body("successful[4]", Matchers.equalTo(true));*/
-  }
-
-  @AfterClass
-  public static void cleanUp() throws Exception {
-    smartServer.cleanUp();
-    cluster.cleanUp();
   }
 }
