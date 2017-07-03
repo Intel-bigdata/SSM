a39a5f7b1de9ef7830d0547d4e82dc48c3c39dc7
timmyyao <shunyang.yao@intel.com>
2017-07-03 18:06:28 +0800

    Refactor some rest api and tests (#547)
    * refactor
    
    * refactor some Rest Api and tests

--- smart-integration/src/main/java/org/smartdata/integration/rest/ActionRestApi.java
+++ smart-integration/src/main/java/org/smartdata/integration/rest/ActionRestApi.java
@@ -44,6 +44,7 @@ public class ActionRestApi extends RestApiBase {
    */
   public static List<Long> getActionIds(long cid) {
     Response cmdletInfo = RestAssured.get(CMDLETROOT + "/" + cid + "/info");
+    cmdletInfo.then().body("status", Matchers.equalTo("OK"));
     JsonPath cmdletInfoPath = new JsonPath(cmdletInfo.asString());
     List<Long> ret = new ArrayList<>();
     for (Object obj: (List)cmdletInfoPath.getMap("body").get("aids")) {
@@ -52,8 +53,14 @@ public class ActionRestApi extends RestApiBase {
     return ret;
   }
 
+  /**
+   * Get action info of an action.
+   * @param aid
+   * @return
+   */
   public static Map getActionInfo(long aid) {
     Response actionInfo = RestAssured.get(ACTIONROOT + "/" + aid + "/info");
+    actionInfo.then().body("status", Matchers.equalTo("OK"));
     JsonPath actionInfoPath = new JsonPath(actionInfo.asString());
     Map actionInfoMap = actionInfoPath.getMap("body");
     return actionInfoMap;
@@ -65,6 +72,7 @@ public class ActionRestApi extends RestApiBase {
    */
   public static List<String> getActionsSupported() {
     Response response = RestAssured.get(ACTIONROOT + "/registry/list");
+    response.then().body("status", Matchers.equalTo("OK"));
     return response.jsonPath().getList("body");
   }
 }
