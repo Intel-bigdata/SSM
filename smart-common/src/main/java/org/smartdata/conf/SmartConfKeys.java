966517aa9c1a0e9c966ee360eea6812494bccfac
littlezhou <wei.zhou@intel.com>
2017-07-14 22:47:40 +0800

    Fix #730, Ignore access events for files under certain directories (#731)

--- smart-common/src/main/java/org/smartdata/conf/SmartConfKeys.java
+++ smart-common/src/main/java/org/smartdata/conf/SmartConfKeys.java
@@ -61,4 +61,8 @@ public class SmartConfKeys {
   // SmartAgent
   public static final String SMART_AGENT_MASTER_ADDRESS_KEY = "smart.agent.master.address";
   public static final String SMART_AGENT_ADDRESS_KEY = "smart.agent.address";
+
+  // SmartClient
+  // Comma delimited directories, access event of files under these directories will not be reported to SSM.
+  public static final String SMART_CLIENT_IGNORE_ACCESS_EVENT_DIRS_KEY = "smart.client.ignore.access.event.dirs";
 }
