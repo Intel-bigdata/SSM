a98687ef1e73054c77da7b061250df2009b6423c
timmyyao <shunyang.yao@intel.com>
2017-07-24 13:03:15 +0800

    Mover executor (#766)
    * Initial code for seperating Mover scheduler and executor
    
    * mover scheduler not finished
    
    * refactor MoverProcessor to two parts
    
    * MoverExecutor

---
+++ smart-actions/src/main/java/org/smartdata/actions/hdfs/move/StorageGroup.java
@@ -0,0 +1,72 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.smartdata.actions.hdfs.move;
+
+import org.apache.hadoop.fs.StorageType;
+import org.apache.hadoop.hdfs.protocol.DatanodeInfo;
+
+/** A group of storages in a datanode with the same storage type. */
+public class StorageGroup {
+  final StorageType storageType;
+  private Dispatcher.DDatanode datanode;
+
+  StorageGroup(Dispatcher.DDatanode datanode, StorageType storageType) {
+    this.datanode = datanode;
+    this.storageType = storageType;
+  }
+
+  public StorageType getStorageType() {
+    return storageType;
+  }
+
+  public Dispatcher.DDatanode getDDatanode() {
+    return datanode;
+  }
+
+  public DatanodeInfo getDatanodeInfo() {
+    return datanode.datanode;
+  }
+
+  /** @return the name for display */
+  String getDisplayName() {
+    return datanode + ":" + storageType;
+  }
+
+  @Override
+  public String toString() {
+    return getDisplayName();
+  }
+
+  @Override
+  public int hashCode() {
+    return getStorageType().hashCode() ^ getDatanodeInfo().hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    } else if (obj == null || !(obj instanceof StorageGroup)) {
+      return false;
+    } else {
+      final StorageGroup that = (StorageGroup) obj;
+      return this.getStorageType() == that.getStorageType()
+          && this.getDatanodeInfo().equals(that.getDatanodeInfo());
+    }
+  }
+}
