fa9622b9b703017273c9f98ae68ae01a5fbeba4b
timmyyao <shunyang.yao@intel.com>
2017-07-25 16:54:13 +0800

    MoverExecutor milestone

--- smart-actions/src/test/java/org/smartdata/actions/hdfs/move/TestMoverExecutor.java
+++ smart-actions/src/test/java/org/smartdata/actions/hdfs/move/TestMoverExecutor.java
@@ -25,11 +25,13 @@ import org.apache.hadoop.hdfs.protocol.DatanodeInfo;
 import org.apache.hadoop.hdfs.protocol.DatanodeInfoWithStorage;
 import org.apache.hadoop.hdfs.protocol.ExtendedBlock;
 import org.apache.hadoop.hdfs.protocol.LocatedBlock;
+import org.apache.hadoop.hdfs.server.protocol.DatanodeStorageReport;
 import org.junit.Assert;
 import org.junit.Test;
 import org.smartdata.actions.hdfs.ActionMiniCluster;
 
 import java.net.URI;
+import java.util.HashSet;
 
 /**
  * Test for MoverExecutor.
@@ -53,16 +55,12 @@ public class TestMoverExecutor extends ActionMiniCluster {
     Configuration conf = smartContext.getConf();
     URI namenode = cluster.getURI();
 
-    String content = "This is a block with 50B." +
-        "This is a block with 50B." +
-        "This is a block with 50B." +
-        "This is a block with 50B." +
-        "This is a block with 50B." +
-        "This is a block with 50B." +
-        "This is a block with 50B." +
-        "This is a block with 50B." +
-        "This is a block with 50B." +
-        "This is a block with 50B.";
+    String blockContent = "This is a block with 50B.";
+    StringBuilder stringBuilder = new StringBuilder();
+    for (int i = 0; i < 50; i ++) {
+      stringBuilder.append(blockContent);
+    }
+    String content = stringBuilder.toString();
     generateFile(content);
 
     SchedulePlan plan = new SchedulePlan(namenode, fileName);
@@ -71,16 +69,16 @@ public class TestMoverExecutor extends ActionMiniCluster {
     for (LocatedBlock lb : MoverExecutor.getLocatedBlocks(dfsClient, fileName)) {
       ExtendedBlock block = lb.getBlock();
       for (DatanodeInfo datanodeInfo : lb.getLocations()) {
-        Dispatcher.DDatanode datanode = new Dispatcher.DDatanode(datanodeInfo, 100);
-        StorageGroup source = new StorageGroup(datanode, StorageType.DISK);
-        StorageGroup target = new StorageGroup(datanode, StorageType.SSD);
+        StorageGroup source = new StorageGroup(datanodeInfo, StorageType.DISK);
+        StorageGroup target = new StorageGroup(datanodeInfo, StorageType.SSD);
         plan.addPlan(source, target, block);
       }
     }
 
     // Do move executor
-    MoverExecutor moverExecutor = new MoverExecutor(conf, 500);
+    MoverExecutor moverExecutor = new MoverExecutor(conf, 10, 500);
     int failedMoves = moverExecutor.executeMove(plan);
+    Assert.assertEquals(0, failedMoves);
 
     // Check storage after move
     for (LocatedBlock lb : MoverExecutor.getLocatedBlocks(dfsClient, fileName)) {
@@ -92,15 +90,58 @@ public class TestMoverExecutor extends ActionMiniCluster {
   }
 
   @Test
+  // TODO: seems the original replica is not deleted after move
+  // DataXceiver.replaceBlock doing this job
   public void moveCrossNodes() throws Exception {
     Configuration conf = smartContext.getConf();
     URI namenode = cluster.getURI();
 
-    //generateFile();
+    generateFile("One-block file");
 
     SchedulePlan plan = new SchedulePlan(namenode, fileName);
 
-    // Move one replica to another node
+    // Schedule move of one replica to another node
+    NameNodeConnector nnc = new NameNodeConnector(namenode, conf);
+    HashSet<DatanodeInfo> fileNodes = new HashSet<>();
+    ExtendedBlock block = null;
+    for (LocatedBlock lb : MoverExecutor.getLocatedBlocks(dfsClient, fileName)) {
+      block = lb.getBlock();
+      for (DatanodeInfo datanodeInfo : lb.getLocations()) {
+        fileNodes.add(datanodeInfo);
+      }
+    }
+    final DatanodeStorageReport[] reports = nnc.getLiveDatanodeStorageReport();
+    for (DatanodeStorageReport report : reports) {
+      DatanodeInfo targetDatanode = report.getDatanodeInfo();
+      if (!fileNodes.contains(targetDatanode)) {
+        StorageGroup source = new StorageGroup(fileNodes.iterator().next(), StorageType.DISK);
+        StorageGroup target = new StorageGroup(targetDatanode, StorageType.SSD);
+        plan.addPlan(source, target, block);
+        break;
+      }
+    }
 
+    // Do mover executor
+    MoverExecutor moverExecutor = new MoverExecutor(conf, 10, 500);
+    int failedMoves = moverExecutor.executeMove(plan);
+    Assert.assertEquals(0, failedMoves);
+
+    // Check storage after move
+    //Thread.sleep(100000);
+    int ssdNum = 0;
+    int hddNum = 0;
+    for (LocatedBlock lb : MoverExecutor.getLocatedBlocks(dfsClient, fileName)) {
+      for (DatanodeInfo datanodeInfo : lb.getLocations()) {
+        Assert.assertTrue(datanodeInfo instanceof DatanodeInfoWithStorage);
+        StorageType storageType = ((DatanodeInfoWithStorage)datanodeInfo).getStorageType();
+        if (storageType.equals(StorageType.SSD)) {
+          ssdNum ++;
+        } else if (storageType.equals(StorageType.DISK)) {
+          hddNum ++;
+        }
+      }
+    }
+    Assert.assertEquals(1, ssdNum);
+    Assert.assertEquals(2, hddNum);
   }
 }
