4eb4c410455388504cfc65ab1ffc4dca678c3955
Qiyuan Gong <qiyuan.gong@intel.com>
2017-07-26 10:31:06 +0800

    Copy, Rename and delete FileAction (#773)

--- smart-actions/src/main/java/org/smartdata/actions/hdfs/RenameFileAction.java
+++ smart-actions/src/main/java/org/smartdata/actions/hdfs/RenameFileAction.java
@@ -19,8 +19,8 @@ package org.smartdata.actions.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Options;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.smartdata.actions.ActionException;
@@ -28,31 +28,27 @@ import org.smartdata.actions.Utils;
 import org.smartdata.actions.annotation.ActionSignature;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URI;
 import java.util.Map;
 
 /**
- * An action to copy a single file from src to destination.
+ * An action to rename a single file
  * If dest doesn't contains "hdfs" prefix, then destination will be set to
- * current cluster, i.e., copy between dirs in current cluster.
+ * current cluster.
  * Note that destination should contains filename.
  */
 @ActionSignature(
-    actionId = "copy",
-    displayName = "copy",
-    usage = HdfsAction.FILE_PATH + " $src " + CopyFileAction.DEST_PATH +
-        " $dest " + CopyFileAction.BUF_SIZE + " $size"
+    actionId = "rename",
+    displayName = "rename",
+    usage = HdfsAction.FILE_PATH + " $src" + RenameFileAction.DEST_PATH +
+        " $dest"
 )
-public class CopyFileAction extends HdfsAction {
+public class RenameFileAction extends HdfsAction {
   private static final Logger LOG =
       LoggerFactory.getLogger(CopyFileAction.class);
-  public static final String BUF_SIZE = "-bufSize";
   public static final String DEST_PATH = "-dest";
   private String srcPath;
   private String destPath;
-  private int bufferSize = 64 * 1024;
 
   @Override
   public void init(Map<String, String> args) {
@@ -60,9 +56,6 @@ public class CopyFileAction extends HdfsAction {
     this.srcPath = args.get(FILE_PATH);
     if (args.containsKey(DEST_PATH)) {
       this.destPath = args.get(DEST_PATH);
-    }
-    if (args.containsKey(BUF_SIZE)) {
-      bufferSize = Integer.valueOf(args.get(BUF_SIZE));
     }
   }
 
@@ -77,44 +70,34 @@ public class CopyFileAction extends HdfsAction {
     appendLog(
         String.format("Action starts at %s : Read %s",
             Utils.getFormatedCurrentTime(), srcPath));
-    if (!dfsClient.exists(srcPath)) {
-      throw new ActionException("CopyFile Action fails, file doesn't exist!");
-    }
-    appendLog(
-        String.format("Copy from %s to %s", srcPath, destPath));
-    copySingleFile(srcPath, destPath);
+    //rename
+    renameSingleFile(srcPath, destPath);
   }
 
-  private boolean copySingleFile(String src, String dest) throws IOException {
-    InputStream srcInputStream = null;
-    OutputStream destOutStream = null;
-    try {
-      srcInputStream = dfsClient.open(src);
-      destOutStream = getDestOutPutStream(dest);
-      // Copy from src to dest
-      IOUtils.copyBytes(srcInputStream, destOutStream, bufferSize, false);
-      return true;
-    } finally {
-      IOUtils.closeStream(srcInputStream);
-      IOUtils.closeStream(destOutStream);
-    }
-  }
-
-  private OutputStream getDestOutPutStream(String dest) throws IOException {
-    if (dest.startsWith("hdfs")) {
-      // Copy between different clusters
+  private boolean renameSingleFile(String src,
+      String dest) throws IOException, ActionException {
+    if (dest.startsWith("hdfs") && src.startsWith("hdfs")) {
+      //rename file in the same remote cluster
       // TODO read conf from files
+      //check the file name
+      if (!URI.create(dest).getHost().equals(URI.create(src).getHost())) {
+        throw new ActionException("the file names are not in the same cluster");
+      }
       Configuration conf = new Configuration();
-      // Get OutPutStream from URL
+      //get FileSystem object
       FileSystem fs = FileSystem.get(URI.create(dest), conf);
-      // TODO overwrite or skip
-      // if (fs.exists(new Path(dest))) {
-      //
-      // }
-      return fs.create(new Path(dest), true);
+      return fs.rename(new Path(src), new Path(dest));
+    } else if (!dest.startsWith("hdfs") && !src.startsWith("hdfs")) {
+      //rename file in local cluster and overwrite
+      if (!dfsClient.exists(src)) {
+        throw new ActionException("the source file is not exist");
+      }
+      dfsClient.rename(src, dest, Options.Rename.NONE);
+      return true;
     } else {
-      // Copy between different dirs of the same cluster
-      return dfsClient.create(dest, true);
+      // TODO handle the case when dest prefixed with the default hdfs uri
+      // while src not, the two path are in the same cluster
+      throw new ActionException("the file names are not in the same cluster");
     }
   }
 }
