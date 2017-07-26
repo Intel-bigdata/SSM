1c27ec648da2bf96c884108f5b10c539b0c9df5d
Qiyuan Gong <qiyuan.gong@intel.com>
2017-07-26 10:31:06 +0800

    Copy, Rename and delete FileAction (#773)

--- smart-actions/src/main/java/org/smartdata/actions/hdfs/HdfsActionFactory.java
+++ smart-actions/src/main/java/org/smartdata/actions/hdfs/HdfsActionFactory.java
@@ -35,7 +35,9 @@ public class HdfsActionFactory extends AbstractActionFactory {
     addAction(CheckStorageAction.class);
 //    addAction("stripec", StripErasureCodeFileAction.class);
 //    addAction("blockec", BlockErasureCodeFileAction.class);
-//    addAction("copy", CopyFileAction.class);
+    addAction(CopyFileAction.class);
+    addAction(DeleteFileAction.class);
+    addAction(RenameFileAction.class);
 //    addAction("list", ListFileAction.class);
 //    addAction("fsck", FsckAction.class);
 //    addAction("diskbalance", DiskBalanceAction.class);
