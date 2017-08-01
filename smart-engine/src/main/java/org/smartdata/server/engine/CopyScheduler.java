2df369313a1d5b7cb3b069d7d5c00db59d04d157
ZhenDu <454858191@qq.com>
2017-08-01 20:28:11 +0800

    Fix #824, add splitCopyFile to DRManager (#833)

--- smart-engine/src/main/java/org/smartdata/server/engine/CopyScheduler.java
+++ smart-engine/src/main/java/org/smartdata/server/engine/CopyScheduler.java
@@ -17,6 +17,9 @@
  */
 package org.smartdata.server.engine;
 
+import org.apache.hadoop.fs.BlockLocation;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import com.google.common.annotations.VisibleForTesting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +31,7 @@ import org.smartdata.model.FileDiff;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.Executors;
@@ -81,6 +85,66 @@ public class CopyScheduler extends AbstractService {
     executorService.shutdown();
   }
 
+  /**
+   * @param sourceFile the source file we want to copy
+   * @param destFile   destination to save the different chunk of source file
+   * @return a list of copy task
+   */
+  static public List<CopyTargetTask> splitCopyFile(String sourceFile, String destFile, int blockPerchunk, FileSystem fileSystem)
+      throws IOException {
+    if (blockPerchunk <= 0) {
+      throw new IllegalArgumentException("the block per chunk must more than 0");
+    }
+    if (sourceFile == null) {
+      throw new IllegalArgumentException("the source file can't be empty");
+    }
+    if (destFile == null) {
+      throw new IllegalArgumentException("the dest file can't be empty");
+    }
+
+    List<CopyTargetTask> copyTargetTaskList = new LinkedList<>();
+
+    //split only in source file is large than a chunk
+    if ((blockPerchunk > 0) &&
+        !fileSystem.getFileStatus(new Path(sourceFile)).isDirectory() &&
+        (fileSystem.getFileStatus(new Path(sourceFile)).getLen() >
+            fileSystem.getFileStatus(new Path(sourceFile)).getBlockSize() * blockPerchunk)) {
+      //here we can split
+      final BlockLocation[] blockLocations;
+      blockLocations = fileSystem.getFileBlockLocations(fileSystem.getFileStatus(new Path(sourceFile)), 0,
+          fileSystem.getFileStatus(new Path(sourceFile)).getLen());
+
+      int numBlocks = blockLocations.length;
+
+      if (numBlocks <= blockPerchunk) {
+        //if has only one chunk
+        copyTargetTaskList.add(new CopyTargetTask(destFile, sourceFile, 0,
+            fileSystem.getFileStatus(new Path(sourceFile)).getLen()));
+      }else {
+        //has many chunk
+        int i = 0;
+        int chunkCount = 0;
+        int position = 0;
+        while (i < numBlocks) {
+          //the length we need to set in copy task
+          long curLength = 0;
+          for (int j = 0; j < blockPerchunk && i < numBlocks; ++j, ++i) {
+            curLength += blockLocations[i].getLength();
+          }
+          if (curLength > 0) {
+            chunkCount++;
+            CopyTargetTask task = new CopyTargetTask(destFile + "_temp_chunkCount" + chunkCount, sourceFile,
+                position, curLength);
+            copyTargetTaskList.add(task);
+            position += curLength;
+          }
+        }
+      }
+    }else {
+      throw new IllegalArgumentException("Incorrect input");
+    }
+    return copyTargetTaskList;
+  }
 
   private class ScheduleTask implements Runnable {
 
@@ -103,6 +167,8 @@ public class CopyScheduler extends AbstractService {
         runningDR.add(cid);
       }
     }
+
+
 
     @Override
     public void run() {
