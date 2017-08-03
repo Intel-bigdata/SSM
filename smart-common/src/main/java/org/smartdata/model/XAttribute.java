b600b416fded3866f5dd17b8d582bf9dfb3342c8
huafengw <fvunicorn@gmail.com>
2017-08-03 14:56:14 +0800

    remove hdfs dependency from metastore

---
+++ smart-common/src/main/java/org/smartdata/model/XAttribute.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.smartdata.model;
+
+import java.util.Arrays;
+
+public class XAttribute {
+  private final String nameSpace;
+  private final String name;
+  private final byte[] value;
+
+  public XAttribute(String nameSpace, String name, byte[] value) {
+    this.nameSpace = nameSpace;
+    this.name = name;
+    this.value = value;
+  }
+
+  public String getNameSpace() {
+    return nameSpace;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public byte[] getValue() {
+    return value;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    XAttribute that = (XAttribute) o;
+
+    if (!nameSpace.equals(that.nameSpace)) return false;
+    if (!name.equals(that.name)) return false;
+    return Arrays.equals(value, that.value);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = nameSpace.hashCode();
+    result = 31 * result + name.hashCode();
+    result = 31 * result + Arrays.hashCode(value);
+    return result;
+  }
+}
