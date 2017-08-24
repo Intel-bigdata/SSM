51b8d33cdcb98162d8c0bc521d3cb5ce2aea721d
Qiyuan Gong <qiyuan.gong@intel.com>
2017-08-24 15:43:43 +0800

    Modify Integration Test README.md

--- supports/integrate-test/README.md
+++ supports/integrate-test/README.md
@@ -1,9 +1,64 @@
-# Integrate Test
+# Integration Test 
+
 
 ## Pre-requests
-
 ### Python Environment
-Python 2.6 or higher
+Python 2.6 or higher, `requests`.
+```
+python --version
 pip install requests
+```
 
-### HDFS Environment
+### HDFS and SSM Environment
+Make sure SSM and HDFS are correctly installed. Before executing test script, please make sure SSM address is correctly configured in `util.py`.
+```
+BASE_URL = "http://{SSM_Server}:7045"
+```
+
+Here `{SSM_Server}` is the IP address of SSM.
+
+## Init/Rest Test Environment
+1. Init/Rest SSM
+```
+bin/init-ssm.sh
+```
+
+2. Remove all files in hdfs:/test/
+Run this command in HDFS enviroment.
+```
+HDFS dfs -ls -rm -r /test/
+```
+
+3. Create 10000 files (1MB) in hdfs:/test/
+
+```
+python reset_env.py -v
+```
+
+## Run Test Scripts
+### Test Mover
+Run all mover test cases with the following command:
+```
+python test_mover.py -v
+```
+
+### Test Rule
+Run all rule test cases with the following command:
+```
+python test_rule.py -v
+```
+
+### Test Data Protection
+Run all data protection test cases with the following command:
+```
+python test_mover_protection.py -v
+```
+
+### Test Stress/Performance
+Run all stress/performance test cases with the following command:
+```
+python test_stress.py -v
+```
+
+
+
--- supports/integrate-test/reset_env.py
+++ supports/integrate-test/reset_env.py
@@ -4,26 +4,37 @@ from util import *
 
 class ResetEnv(unittest.TestCase):
 
-    def test_delete_all_rules(self):
-        """
-        delete all rules
-        """
-        rules = list_rule()
-        for rule in rules:
-            # Delete all rules
-            if rule['state'] != 'DELETED':
-                delete_rule(rule['id'])
-        rules = [r for rule in list_rule() if rule['state'] != 'DELETED']
-        self.assertTrue(len(rules) == 0)
+    # def test_delete_all_rules(self):
+    #     """
+    #     delete all rules
+    #     """
+    #     rules = list_rule()
+    #     for rule in rules:
+    #         # Delete all rules
+    #         if rule['state'] != 'DELETED':
+    #             delete_rule(rule['id'])
+    #     rules = [r for rule in list_rule() if rule['state'] != 'DELETED']
+    #     self.assertTrue(len(rules) == 0)
 
-    def test_delete_all_actions(self):
-        """
-        delete all actions, currently not supported
-        """
-        pass
+    # def test_delete_all_actions(self):
+    #     """
+    #     delete all actions, currently not supported
+    #     """
+    #     pass
 
-    def test_delete_all_files(self):
-        subprocess.call("sh init_HDFS", shell=True)
+    # def test_delete_all_files(self):
+    #     subprocess.call("sh init_HDFS", shell=True)
+
+    def test_create_10000_files(self):
+        max_number = 10000
+        file_paths = []
+        cids = []
+        for i in range(max_number):
+            # 1 MB files
+            file_path, cid = create_random_file_parallel(1024 * 1024)
+            file_paths.append(file_path)
+            cids.append(cid)
+        wait_for_cmdlets(cids)
 
 
 if __name__ == '__main__':
