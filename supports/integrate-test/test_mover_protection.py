6505183d731ea91a8e438456f0429052b50bc8b0
ZhenDu <454858191@qq.com>
2017-08-24 15:06:43 +0800

    fix some bug

--- supports/integrate-test/test_mover_protection.py
+++ supports/integrate-test/test_mover_protection.py
@@ -9,11 +9,11 @@ class TestMoverProtection(unittest.TestCase):
         # Test with 2 GB file
         file_path = create_random_file(1024 * 1024 * 1024)
         # Begin to move file
-        cid_moves = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[1]], file_path))
+        cid_move = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[1]], file_path))
         # read the file
         cid_read = read_file(file_path)
         # check the statement of read
-        self.assertTrue(all_success(cid_moves))
+        self.assertTrue(all_success([cid_move]))
         self.assertTrue(all_success([cid_read]))
 
     def test_mover_onessd_read(self):
@@ -22,11 +22,11 @@ class TestMoverProtection(unittest.TestCase):
         # Test with 2 GB file
         file_path = create_random_file(1024 * 1024 * 1024)
         # Begin to move file
-        cid_moves = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[0]], file_path))
+        cid_move = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[0]], file_path))
         # read the file
         cid_read = read_file(file_path)
         # check the statement of read
-        self.assertTrue(all_success(cid_moves))
+        self.assertTrue(all_success([cid_move]))
         self.assertTrue(all_success([cid_read]))
 
     def test_mover_archive_read(self):
@@ -35,11 +35,11 @@ class TestMoverProtection(unittest.TestCase):
         # Test with 2 GB file
         file_path = create_random_file(1024 * 1024 * 1024)
         # Begin to move file
-        cid_moves = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[2]], file_path))
+        cid_move = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[2]], file_path))
         # read the file
         cid_read = read_file(file_path)
         # check the statement of read
-        self.assertTrue(all_success(cid_moves))
+        self.assertTrue(all_success([cid_move]))
         self.assertTrue(all_success([cid_read]))
 
     def test_mover_allssd_delete(self):
@@ -47,11 +47,11 @@ class TestMoverProtection(unittest.TestCase):
         # print check_storage("/testFile")
         # Test with 2 GB file
         file_path = create_random_file(1024 * 1024 * 1024)
-        cid_moves = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[1]], file_path))
+        cid_move = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[1]], file_path))
         # delete the file
         cid_delete = delete_file(file_path)
         # check the statement of read
-        self.assertTrue(all_success(cid_moves))
+        self.assertTrue(all_success([cid_move]))
         self.assertTrue(all_success([cid_delete]))
 
     def test_mover_onessd_delete(self):
@@ -59,11 +59,11 @@ class TestMoverProtection(unittest.TestCase):
         # print check_storage("/testFile")
         # Test with 2 GB file
         file_path = create_random_file(1024 * 1024 * 1024)
-        cid_moves = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[0]], file_path))
+        cid_move = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[0]], file_path))
         # delete the file
         cid_delete = delete_file(file_path)
         # check the statement of read
-        self.assertTrue(all_success(cid_moves))
+        self.assertTrue(all_success([cid_move]))
         self.assertTrue(all_success([cid_delete]))
 
     def test_mover_archive_delete(self):
@@ -71,11 +71,11 @@ class TestMoverProtection(unittest.TestCase):
         # print check_storage("/testFile")
         # Test with 2 GB file
         file_path = create_random_file(1024 * 1024 * 1024)
-        cid_moves = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[2]], file_path))
+        cid_move = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[2]], file_path))
         # delete the file
         cid_delete = delete_file(file_path)
         # check the statement of read
-        self.assertTrue(all_success(cid_moves))
+        self.assertTrue(all_success([cid_move]))
         self.assertTrue(all_success([cid_delete]))
 
     def test_mover_allssd_append(self):
@@ -83,11 +83,11 @@ class TestMoverProtection(unittest.TestCase):
         # print check_storage("/testFile")
         # Test with 2 GB file
         file_path = create_random_file(1024 * 1024 * 1024)
-        cid_moves = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[1]], file_path))
+        cid_move = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[1]], file_path))
         # append random content to current file
         cid_append = append_file(file_path, random.randrange(1024, 1024 * 1024 * 2))
         # check the statement of read
-        self.assertTrue(all_success(cid_moves))
+        self.assertTrue(all_success([cid_move]))
         self.assertTrue(all_success([cid_append]))
 
     def test_mover_onessd_append(self):
@@ -95,11 +95,11 @@ class TestMoverProtection(unittest.TestCase):
         # print check_storage("/testFile")
         # Test with 2 GB file
         file_path = create_random_file(1024 * 1024 * 1024)
-        cid_moves = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[0]], file_path))
+        cid_move = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[0]], file_path))
         # append random content to current file
         cid_append = append_file(file_path, random.randrange(1024, 1024 * 1024 * 2))
         # check the statement of read
-        self.assertTrue(all_success(cid_moves))
+        self.assertTrue(all_success([cid_move]))
         self.assertTrue(all_success([cid_append]))
 
     def test_mover_archive_append(self):
@@ -107,11 +107,11 @@ class TestMoverProtection(unittest.TestCase):
         # print check_storage("/testFile")
         # Test with 2 GB file
         file_path = create_random_file(1024 * 1024 * 1024)
-        cid_moves = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[2]], file_path))
+        cid_move = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[2]], file_path))
         # append random content to current file
         cid_append = append_file(file_path, random.randrange(1024, 1024 * 1024 * 2))
         # check the statement of read
-        self.assertTrue(all_success(cid_moves))
+        self.assertTrue(all_success([cid_move]))
         self.assertTrue(all_success([cid_append]))
 
     def test_mover_allssd_overwrite(self):
@@ -119,12 +119,12 @@ class TestMoverProtection(unittest.TestCase):
         # print check_storage("/testFile")
         # Test with 2 GB file
         file_path = create_random_file(1024 * 1024 * 1024)
-        cid_moves = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[1]], file_path))
+        cid_move = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[1]], file_path))
         check_storage(file_path)
         # overwrite the file
         cid_delete = create_file(file_path, 24 * 1024 * 1024)
         # check the statement of read
-        self.assertTrue(all_success(cid_moves))
+        self.assertTrue(all_success([cid_move]))
         self.assertTrue(all_success([cid_delete]))
 
     def test_mover_onessd_overwrite(self):
@@ -132,11 +132,11 @@ class TestMoverProtection(unittest.TestCase):
         # print check_storage("/testFile")
         # Test with 2 GB file
         file_path = create_random_file(1024 * 1024 * 1024)
-        cid_moves = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[0]], file_path))
+        cid_move = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[0]], file_path))
         # overwrite the file
         cid_delete = create_file(file_path, 24 * 1024 * 1024)
         # check the statement of read
-        self.assertTrue(all_success(cid_moves))
+        self.assertTrue(all_success([cid_move]))
         self.assertTrue(all_success([cid_delete]))
 
     def test_mover_archive_overwrite(self):
@@ -144,11 +144,11 @@ class TestMoverProtection(unittest.TestCase):
         # print check_storage("/testFile")
         # Test with 2 GB file
         file_path = create_random_file(1024 * 1024 * 1024)
-        cid_moves = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[2]], file_path))
+        cid_move = wait_for_cmdlet(move_cmdlet([MOVE_TYPE[2]], file_path))
         # overwrite the file
         cid_delete = create_file(file_path, 24 * 1024 * 1024)
         # check the statement of read
-        self.assertTrue(all_success(cid_moves))
+        self.assertTrue(all_success([cid_move]))
         self.assertTrue(all_success([cid_delete]))
 
 if __name__ == '__main__':
