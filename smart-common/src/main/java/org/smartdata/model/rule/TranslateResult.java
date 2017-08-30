35c1058ba5f5f3d1f24650cf45d342465008fb2a
littlezhou <wei.zhou@intel.com>
2017-08-30 10:11:46 +0800

    Solve #1015, Add interface to impact the execution of rules (#1016)

--- smart-common/src/main/java/org/smartdata/model/rule/TranslateResult.java
+++ smart-common/src/main/java/org/smartdata/model/rule/TranslateResult.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.smartdata.rule.parser;
+package org.smartdata.model.rule;
 
 import org.smartdata.model.CmdletDescriptor;
 
