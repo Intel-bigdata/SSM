9b263c518fe5bb30864e0ea759b2eac596efd460
Qiyuan Gong <qiyuan.gong@intel.com>
2017-09-04 17:07:32 +0800

    Refine sync action

--- smart-action/src/main/java/org/smartdata/action/SyncAction.java
+++ smart-action/src/main/java/org/smartdata/action/SyncAction.java
@@ -1,12 +1,28 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.smartdata.action;
 
 import org.smartdata.action.annotation.ActionSignature;
 
 /**
  * Sync action is an abstract action for backup and copy
- * Users can submit a sync action with detailed src (path) and optional
- * dest(path), e.g., "sync -src /test/1 -dest hdfs:/remoteIP:port/test/1",
- * "sync -src /test/1, if backup rule scanning /test is configured.
+ * Users can submit a sync action with detailed src path and
+ * dest path, e.g., "sync -src /test/1 -dest hdfs:/remoteIP:port/test/1"
  */
 @ActionSignature(
     actionId = "sync",
@@ -14,23 +30,12 @@ import org.smartdata.action.annotation.ActionSignature;
     usage = SyncAction.SRC + " $src" + SyncAction.DEST + " $dest"
 )
 public class SyncAction extends SmartAction {
-
-  // TODO add dir support
   // related to fileDiff.src
   public static final String SRC = "-src";
-  // optional, related to remote cluster and fileDiff.src
+  // related to remote cluster and fileDiff.src
   public static final String DEST = "-dest";
 
   @Override
   protected void execute() throws Exception {
-    this.appendResult(String
-        .format("Sync %s", getArguments().get(SRC)));
-    if (getArguments().containsKey(DEST)) {
-      this.appendResult(String
-          .format(" to %s\n",
-              getArguments().get(DEST)));
-    } else {
-      this.appendResult("\n");
-    }
   }
 }
