83c255c2ac6979d1726f86493633dbe65e81a7bf
Qiyuan Gong <qiyuan.gong@intel.com>
2017-09-04 13:34:26 +0800

    Add src and dest support

--- smart-action/src/main/java/org/smartdata/action/SyncAction.java
+++ smart-action/src/main/java/org/smartdata/action/SyncAction.java
@@ -4,23 +4,33 @@ import org.smartdata.action.annotation.ActionSignature;
 
 /**
  * Sync action is an abstract action for backup and copy
+ * Users can submit a sync action with detailed src (path) and dest(path),
+ * e.g., sync -src /test/1 -dest hdfs:/remoteIP:port/test/1
  */
 @ActionSignature(
     actionId = "sync",
     displayName = "sync",
-    usage = SyncAction.FILE + " $file" + SyncAction.DIFF + " $diffId"
+    usage = SyncAction.SRC + " $src" + SyncAction.DEST + " $dest"
 )
 public class SyncAction extends SmartAction {
 
+  // TODO add dir support
   // related to fileDiff.src
-  public static final String FILE = "-file";
-  // related to fileDiff.diffId
+  public static final String SRC = "-src";
+  // related to remote cluster + fileDiff.src
+  public static final String DEST = "-dest";
+  // optional, related to fileDiff.diffId
   public static final String DIFF = "-diff";
 
   @Override
   protected void execute() throws Exception {
     this.appendResult(String
-        .format("Sync %s based on file diff %s\n", getArguments().get(FILE),
-            getArguments().get(DIFF)));
+        .format("Sync from %s to %s\n", getArguments().get(SRC),
+            getArguments().get(DEST)));
+    if (getArguments().containsKey(DIFF)) {
+      this.appendResult(String
+          .format("Sync based on file diff %s\n",
+              getArguments().get(DIFF)));
+    }
   }
 }
