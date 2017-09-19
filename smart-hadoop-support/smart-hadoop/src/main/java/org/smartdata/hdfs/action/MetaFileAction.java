77a7bbebf769e8b8c770af9b18037fbef64d07cf
ZhenDu <454858191@qq.com>
2017-09-19 16:27:43 +0800

    add MetaFileAction

---
+++ smart-hadoop-support/smart-hadoop/src/main/java/org/smartdata/hdfs/action/MetaFileAction.java
@@ -0,0 +1,87 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.smartdata.hdfs.action;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smartdata.action.Utils;
+import org.smartdata.action.annotation.ActionSignature;
+import org.smartdata.model.FileInfo;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * action to set MetaData of file
+ */
+@ActionSignature(
+    actionId = "metadata",
+    displayName = "metadata",
+    usage = HdfsAction.FILE_PATH + " $src "
+)
+public class MetaFileAction extends HdfsAction {
+  private static final Logger LOG = LoggerFactory.getLogger(MetaFileAction.class);
+  public static final String OWNER_NAME = "-owername";
+  public static final String GROUP_NAME = "-groupname";
+  private String srcPath;
+
+
+  @Override
+  protected void execute() throws Exception {
+    if (srcPath == null) {
+      throw new IllegalArgumentException("File src is missing.");
+    }
+
+
+  }
+
+  private boolean changeFileMetaData(String srcFile, FileInfo fileInfo) throws IOException {
+    if (srcFile.startsWith("hdfs")) {
+      //change file metadata in remote cluster
+      // TODO read conf from files
+      Configuration conf = new Configuration();
+      FileSystem fs = FileSystem.get(URI.create(srcFile), conf);
+
+      if (fileInfo.getOwner() != null) {
+        fs.setOwner(new Path(srcFile), fileInfo.getOwner(), fs.getFileStatus(new Path(srcFile)).getGroup());
+      }
+
+      if (fileInfo.getGroup() != null) {
+        fs.setOwner(new Path(srcFile), fs.getFileStatus(new Path(srcFile)).getOwner(), fileInfo.getGroup());
+      }
+
+      return true;
+    } else {
+
+
+      return true;
+    }
+
+    return false;
+  }
+}
