8d0b7b48044fd5eb18d0acfa61d168fb6f8c62f1
PHILO-HE <feilong.he@intel.com>
2017-09-23 14:43:34 +0800

    Get the status of pd, tikv and tidb to coordinate these servers (#1142)

--- smart-tidb/src/main/java/org/smartdata/tidb/LaunchDB.java
+++ smart-tidb/src/main/java/org/smartdata/tidb/LaunchDB.java
@@ -20,28 +20,52 @@ package org.smartdata.tidb;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Launch implements Runnable {
-  private final static Logger LOG = LoggerFactory.getLogger(Launch.class);
+public class LaunchDB implements Runnable {
+  private PdServer pdServer;
+  private TikvServer tikvServer;
+  private TidbServer tidbServer;
+  private boolean dbReady = false;
+  private final static Logger LOG = LoggerFactory.getLogger(LaunchDB.class);
 
-  public void run() {
+  public LaunchDB() {
     String pdArgs = new String("--data-dir=pd --log-file=logs/pd.log");
     String tikvArgs = new String("--pd=127.0.0.1:2379 --data-dir=tikv --log-file=logs/tikv.log");
-    //String tidbArgs = new String("--store=tikv --path=127.0.0.1:2379 --log-file=logs/tidb.log");
-    String tidbArgs = new String("--log-file=logs/tidb.log");
+    //The default lease time is 10s. Setting a smaller value may decrease the time of executing ddl statement, but it's dangerous to do so.
+    String tidbArgs = new String("--store=tikv --path=127.0.0.1:2379 --lease=1s --log-file=logs/tidb.log");
+    //String tidbArgs = new String("--log-file=logs/tidb.log");
 
-    PdServer pdServer = new PdServer(pdArgs);
-    TikvServer tikvServer = new TikvServer(tikvArgs);
-    TidbServer tidbServer = new TidbServer(tidbArgs);
+    pdServer = new PdServer(pdArgs);
+    tikvServer = new TikvServer(tikvArgs);
+    tidbServer = new TidbServer(tidbArgs);
+  }
 
+  public boolean isCompleted() {
+    return dbReady;
+  }
+
+  public void run() {
     Thread pdThread = new Thread(pdServer);
     pdThread.start();
     try {
-      Thread.sleep(4000);
+      while (!pdServer.isReady()) {
+        Thread.sleep(100);
+      }
+      LOG.info("Pd server is ready.");
+
       Thread tikvThread = new Thread(tikvServer);
       tikvThread.start();
-      Thread.sleep(6000);
+      while (!tikvServer.isReady()) {
+        Thread.sleep(100);
+      }
+      LOG.info("Tikv server is ready.");
+
       Thread tidbThread = new Thread(tidbServer);
       tidbThread.start();
+      while (!tidbServer.isReady()) {
+        Thread.sleep(100);
+      }
+      LOG.info("Tidb server is ready.");
+      dbReady = true;
     } catch (InterruptedException ex) {
       LOG.error(ex.getMessage());
     }
