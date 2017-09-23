9b22d4a91c2de0784e3ddba35f82dc142da22352
Huafeng Wang <fvunicorn@gmail.com>
2017-09-24 00:58:57 +0800

    TruncateAction (#1147)
    * Add TruncateAction for primary and remote HDFS.
    * Note that TruncateAction is not supported on Hadoop 2.6.

--- smart-hadoop-support/smart-hadoop-cdh-2.6/src/main/java/org/smartdata/hdfs/CompatibilityHelper26.java
+++ smart-hadoop-support/smart-hadoop-cdh-2.6/src/main/java/org/smartdata/hdfs/CompatibilityHelper26.java
@@ -17,6 +17,8 @@
  */
 package org.smartdata.hdfs;
 
+import org.apache.hadoop.hdfs.DFSClient;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
 import org.apache.hadoop.hdfs.StorageType;
 import org.apache.hadoop.hdfs.inotify.Event;
 import org.apache.hadoop.hdfs.protocol.BlockStoragePolicy;
@@ -31,6 +33,7 @@ import org.apache.hadoop.security.token.Token;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.nio.file.FileSystem;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -108,5 +111,15 @@ public class CompatibilityHelper26 implements CompatibilityHelper {
   @Override
   public Event.AppendEvent getAppendEvent(InotifyProtos.AppendEventProto proto) {
     return new Event.AppendEvent.Builder().path(proto.getPath()).build();
+  }
+
+  @Override
+  public boolean truncate(DFSClient client, String src, long newLength) throws IOException {
+    throw new UnsupportedOperationException("Hadoop 2.6 does not support truncate.");
+  }
+
+  @Override
+  public boolean truncate(DistributedFileSystem fs, String src, long newLength) throws IOException {
+    throw new UnsupportedOperationException("Hadoop 2.6 does not support truncate.");
   }
 }
