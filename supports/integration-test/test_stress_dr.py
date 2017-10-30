ed413d218cbcdca60855a56d83ca865be63366be
Qiyuan Gong <qiyuan.gong@intel.com>
2017-10-30 20:34:31 +0800

    Add basic DR test

--- supports/integration-test/test_stress_dr.py
+++ supports/integration-test/test_stress_dr.py
@@ -2,62 +2,32 @@ import unittest
 import time
 from util import *
 
+DEST_DIR = "hdfs://datanode3:9000/dest"
+
 
 class TestStressDR(unittest.TestCase):
-
-    # def test_DR_1000(self):
-    #     max_number = 200
-    #     rids = []
-    #     for i in range(max_number):
-    #         rule_str = "file: " + \
-    #             "every 4s from now to now + 1d |" + \
-    #             " path matches " + \
-    #             "\"/test/" + random_string()[:5] + " *\"" + \
-    #             " | onessd "
-    #         rids.append(submit_rule(rule_str))
-    #     # activate all rules
-    #     for rid in rids:
-    #         start_rule(rid)
-    #     # sleep 60s
-    #     time.sleep(60)
-    #     for rid in rids:
-    #         delete_rule(rid)
-
-    # def test_DR_5000(self):
-    #     max_number = 500
-    #     rids = []
-    #     for i in range(max_number):
-    #         rule_str = "file: " + \
-    #             "every 4s from now to now + 1d |" + \
-    #             " path matches " + \
-    #             "\"/test/" + random_string()[:5] + " *\"" + \
-    #             " | onessd "
-    #         rids.append(submit_rule(rule_str))
-    #     # activate all rules
-    #     for rid in rids:
-    #         start_rule(rid)
-    #     # sleep 60s
-    #     time.sleep(60)
-    #     for rid in rids:
-    #         delete_rule(rid)
-
-    # def test_DR_50000(self):
-    #     max_number = 1000
-    #     rids = []
-    #     for i in range(max_number):
-    #         rule_str = "file: " + \
-    #             "every 4s from now to now + 1d |" + \
-    #             " path matches " + \
-    #             "\"/test/" + random_string()[:5] + " *\"" + \
-    #             " | onessd "
-    #         rids.append(submit_rule(rule_str))
-    #     # activate all rules
-    #     for rid in rids:
-    #         start_rule(rid)
-    #     # sleep 60s
-    #     time.sleep(60)
-    #     for rid in rids:
-    #         delete_rule(rid)
+    def test_rule_access_count(self):
+        # file : every 1s | path matches "/1MB/*" | sync -dest
+        # file_path = create_random_file(10 * 1024 * 1024)
+        # submit rule
+        source_dir = "/1MB/"
+        rule_str = "file : every 1s | path matches " + \
+            "\"" + source_dir + "*\" | sync -dest " + DEST_DIR
+        rid = submit_rule(rule_str)
+        # Activate rule
+        start_rule(rid)
+        # Submit read action to trigger rule
+        # Read three times
+        cmds = []
+        cmds.append(read_file(file_path))
+        cmds.append(read_file(file_path))
+        cmds.append(read_file(file_path))
+        wait_for_cmdlets(cmds)
+        time.sleep(15)
+        # Statue check
+        rule = get_rule(rid)
+        self.assertTrue(rule['numCmdsGen'] > 0)
+        delete_rule(rid)
 
 
 if __name__ == '__main__':
