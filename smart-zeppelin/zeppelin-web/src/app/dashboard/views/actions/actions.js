d6b01557947bec8d779118affedeb46cd180669f
zhiqiangx <zhiqiangx.zhang@intel.com>
2017-11-02 20:16:05 +0800

    Solve #1123, Listing Actions very slow when there are more than 10K actions.

--- smart-zeppelin/zeppelin-web/src/app/dashboard/views/actions/actions.js
+++ smart-zeppelin/zeppelin-web/src/app/dashboard/views/actions/actions.js
@@ -19,80 +19,46 @@
 angular.module('zeppelinWebApp')
 
   .controller('ActionsCtrl', ActionsCtrl);
-  ActionsCtrl.$inject = ['$scope', '$modal', '$sortableTableBuilder', '$dialogs', 'actions0', 'actionTypes'];
-  function ActionsCtrl($scope, $modal, $stb, $dialogs, actions0, actionTypes) {
-    'use strict';
-
-    var submitWindow = $modal({
-      templateUrl: 'app/dashboard/views/actions/submit/submit.html',
-      controller: 'ActionSubmitCtrl',
-      backdrop: 'static',
-      keyboard: true,
-      show: false,
-      resolve: {
-        actionTypes: function () {
-          return actionTypes;
-        }
+  ActionsCtrl.$inject = ['$scope', 'baseUrlSrv', '$filter', '$http', 'conf'];
+  function ActionsCtrl($scope, baseUrlSrv, $filter, $http, conf) {
+    $scope.pageNumber = 10;
+    $scope.totalNumber = 0;
+    $scope.actions;
+    $scope.currentPage = 1;
+    $scope.totalPage = 1;
+    $scope.orderby = 'aid';
+    $scope.isDesc = true;
+    function getActions() {
+      $http.get(baseUrlSrv.getRestApiRoot() + 'smart/api/' + conf.restapiProtocol + '/actions/list/'
+        + $scope.currentPage + '/' + $scope.pageNumber + '/' + $scope.orderby + '/' + $scope.isDesc)
+        .then(function(response) {
+        var actionData = angular.fromJson(response.data);
+        $scope.totalNumber = actionData.body.totalNumOfActions;
+        $scope.actions = actionData.body.actions;
+        angular.forEach($scope.actions, function (data,index) {
+          data.runTime = data.finishTime - data.createTime;
+          data.createTime = $filter('date')(data.createTime,'yyyy-MM-dd HH:mm:ss');
+          data.finishTime = data.finished ? $filter('date')(data.finishTime,'yyyy-MM-dd HH:mm:ss') : '-';
+          data.progressColor = data.finished ? data.successful ? 'success' : 'danger' : 'warning';
+          console.log('data.progressColor', data.progressColor);
+        });
+        $scope.totalPage = Math.ceil($scope.totalNumber / $scope.pageNumber);
+      }, function(errorResponse) {
+        // Handle error case
+      });
+    };
+    $scope.gotoPage = function (index) {
+      $scope.currentPage = index;
+      getActions();
+    };
+    $scope.defindOrderBy = function (filed) {
+      if ($scope.orderby === filed) {
+        $scope.isDesc = ! $scope.isDesc;
+      } else {
+        $scope.orderby = filed;
+        $scope.isDesc = true;
       }
-    });
-
-    $scope.openSubmitActionDialog = function () {
-      submitWindow.$promise.then(submitWindow.show);
+      getActions();
     };
-
-    $scope.actionsTable = {
-      cols: [
-        // group 1/3 (4-col)
-        $stb.indicator().key('state').canSort('state.condition+"_"+createTime').styleClass('td-no-padding').done(),
-        $stb.text('ID').key('id').canSort().sortDefaultDescent().done(),
-        $stb.text('Name').key(['actionName']).canSort().done(),
-        $stb.datetime('Create Time').key('createTime').canSort().done(),
-        $stb.datetime('Finish Time').key('finishTime').canSort().done(),
-        $stb.duration("Running Time").key('runningTime').canSort().done(),
-        $stb.text('Status').key('succeed').canSort().styleClass('col-md-1 hidden-sm hidden-xs').done(),
-        $stb.progressbar('Progress').key('progress').sortBy('progress.usage').styleClass('col-md-1').done(),
-        $stb.button('Actions').key(['view']).styleClass('col-md-1').done()
-      ],
-      rows: null
-    };
-
-    function updateTable(actions) {
-      $scope.actionsTable.rows = $stb.$update($scope.actionsTable.rows,
-        _.map(actions, function (action) {
-          return {
-            id: action.actionId,
-            state: {tooltip: action.status, condition: action.finished ? '' : 'good', shape: 'stripe'},
-            actionName: {
-              title: "ID:" + action.actionId + " Cmdlet ID:" + action.cmdletId + " Name:" + action.actionName
-              + " Create Time:" + new Date(action.createTime).toUTCString()
-              + " Finish Time:" + new Date(action.finished ? action.finishTime : "-").toUTCString()
-              + " Running Time:" + action.uptime + "ms"
-              + " Succeed:" + (action.finished ? action.successful : "-"),
-              value: action.actionName
-            },
-            createTime: action.createTime,
-            finishTime: action.finished ? action.finishTime : "-",
-            runningTime: action.finished ? action.uptime : "-",
-            succeed: action.finished ? action.successful ? "Successful" : "Faild" : "-",
-            view: {
-              href: action.pageUrl,
-              icon: function() {
-                return 'glyphicon glyphicon-info-sign';
-              },
-              class: 'btn-xs btn-info'
-            },
-            progress: {
-                current: action.progress,
-                max: 1,
-                flag: action.finished ? action.successful : "-"
-                // usage: action.progress * 100
-            }
-          };
-        }));
-    }
-
-    updateTable(actions0.$data());
-    actions0.$subscribe($scope, function (actions) {
-      updateTable(actions);
-    });
+    getActions();
   }
