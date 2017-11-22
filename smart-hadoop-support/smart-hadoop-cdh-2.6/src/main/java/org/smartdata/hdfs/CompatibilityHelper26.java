d7b7936f68ad8b1032dc499ec8ccb6435600180d
ZhenDu <454858191@qq.com>
2017-11-22 16:42:27 +0800

    Solve #1426, fix compatibility on CDH-5.10 (Hadoop 2.6) (#1427)

--- smart-hadoop-support/smart-hadoop-cdh-2.6/src/main/java/org/smartdata/hdfs/CompatibilityHelper26.java
+++ smart-hadoop-support/smart-hadoop-cdh-2.6/src/main/java/org/smartdata/hdfs/CompatibilityHelper26.java
@@ -17,6 +17,8 @@
  */
 package org.smartdata.hdfs;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.DFSClient;
 import org.apache.hadoop.hdfs.DistributedFileSystem;
 import org.apache.hadoop.hdfs.StorageType;
@@ -36,6 +38,7 @@ import org.apache.hadoop.security.token.Token;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URI;
 import java.nio.file.FileSystem;
 import java.util.ArrayList;
 import java.util.List;
@@ -140,5 +143,16 @@ public class CompatibilityHelper26 implements CompatibilityHelper {
           .append(dest, buffersize, null, null);
     }
     return client.create(dest, true);
+  }
+
+  @Override
+  public OutputStream getS3outputStream(String dest, Configuration conf) throws IOException {
+    // Copy to remote S3
+    if (!dest.startsWith("s3")) {
+      throw new IOException();
+    }
+    // Copy to s3
+    org.apache.hadoop.fs.FileSystem fs = org.apache.hadoop.fs.FileSystem.get(URI.create(dest), conf);
+    return fs.create(new Path(dest), true);
   }
 }
