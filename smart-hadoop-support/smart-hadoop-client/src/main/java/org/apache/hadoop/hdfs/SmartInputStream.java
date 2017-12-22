d85acbb1765ba628009aee89e6528b14f508b3cc
timmyyao <shunyang.yao@intel.com>
2017-12-22 17:29:58 +0800

    Temp realization of SmartInputStream with 2.6 and 2.7

--- smart-hadoop-support/smart-hadoop-client/src/main/java/org/apache/hadoop/hdfs/SmartInputStream.java
+++ smart-hadoop-support/smart-hadoop-client/src/main/java/org/apache/hadoop/hdfs/SmartInputStream.java
@@ -18,6 +18,7 @@
 package org.apache.hadoop.hdfs;
 
 import org.apache.hadoop.fs.UnresolvedLinkException;
+import org.apache.hadoop.util.VersionInfo;
 import org.apache.htrace.TraceScope;
 import org.smartdata.model.FileState;
 
@@ -29,6 +30,9 @@ import java.io.IOException;
 public abstract class SmartInputStream extends DFSInputStream {
   protected final FileState fileState;
 
+  private static final String HADOOP_26_VERSION = "2.6";
+  private static final String HADOOP_27_VERSION = "2.7";
+
   SmartInputStream(DFSClient dfsClient, String src, boolean verifyChecksum,
       FileState fileState) throws IOException, UnresolvedLinkException {
     super(dfsClient, src, verifyChecksum);
@@ -39,12 +43,29 @@ public abstract class SmartInputStream extends DFSInputStream {
     return fileState.getFileType();
   }
 
+  private static String getHadoopVersion() {
+    String version = VersionInfo.getVersion();
+    String[] parts = version.split("\\.");
+    if (parts.length < 2) {
+      throw new RuntimeException("Illegal Hadoop Version: " + version + " (expected A.B.* format)");
+    }
+    Integer first = Integer.parseInt(parts[0]);
+    if (first == 0 || first == 1) {
+      throw new RuntimeException("Hadoop version 0.x and 1.x are not supported");
+    }
+    Integer second = Integer.parseInt(parts[1]);
+    if (first == 2 && second <= 6) {
+      return HADOOP_26_VERSION;
+    } else {
+      return HADOOP_27_VERSION;
+    }
+  }
+
   public static DFSInputStream create(DFSClient dfsClient, String src,
       boolean verifyChecksum, FileState fileState)
       throws IOException, UnresolvedLinkException {
     dfsClient.checkOpen();
-    TraceScope scope = dfsClient.getPathTraceScope("newDFSInputStream", src);
-    try {
+    if (getHadoopVersion().equals(HADOOP_26_VERSION)) {
       DFSInputStream inputStream = null;
       switch (fileState.getFileType()) {
         case NORMAL:
@@ -63,8 +84,30 @@ public abstract class SmartInputStream extends DFSInputStream {
           throw new IOException("Unsupported file type");
       }
       return inputStream;
-    } finally {
-      scope.close();
+    } else {
+      TraceScope scope = dfsClient.getPathTraceScope("newDFSInputStream", src);
+      try {
+        DFSInputStream inputStream = null;
+        switch (fileState.getFileType()) {
+          case NORMAL:
+            inputStream = new DFSInputStream(dfsClient, src, verifyChecksum);
+            break;
+          case COMPACT:
+            inputStream = new CompactInputStream(dfsClient, src, verifyChecksum, fileState);
+            break;
+          case COMPRESSION:
+            inputStream = new CompressionInputStream(dfsClient, src, verifyChecksum, fileState);
+            break;
+          case S3:
+            inputStream = new S3InputStream(dfsClient, src, verifyChecksum, fileState);
+            break;
+          default:
+            throw new IOException("Unsupported file type");
+        }
+        return inputStream;
+      } finally {
+        scope.close();
+      }
     }
   }
 }
