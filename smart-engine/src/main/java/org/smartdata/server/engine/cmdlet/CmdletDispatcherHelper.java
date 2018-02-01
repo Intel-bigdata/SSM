577917e06014bd0655470373da6e90282758e60d
PHILO-HE <feilong.he@intel.com>
2018-02-01 18:10:57 +0800

    Refix bug in CmdletDispatcherHelper

--- smart-engine/src/main/java/org/smartdata/server/engine/cmdlet/CmdletDispatcherHelper.java
+++ smart-engine/src/main/java/org/smartdata/server/engine/cmdlet/CmdletDispatcherHelper.java
@@ -33,8 +33,8 @@ public class CmdletDispatcherHelper {
   private CmdletDispatcher dispatcher = null;
 
   public void register(CmdletDispatcher dispatcher) {
-    this.dispatcher = dispatcher;
     synchronized (msgs) {
+      this.dispatcher = dispatcher;
       for (int i = 0; i < msgs.size(); i++) {
         dispatcher.onNodeMessage(msgs.get(i), opers.get(i));
       }
