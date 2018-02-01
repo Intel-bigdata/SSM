64713039c16d6a52820b6896a698d2266837934d
Polaris Feng <fztfztfztfzt@gmail.com>
2018-02-01 17:04:30 +0800

    fix code style

--- supports/integration-test/pyarrow_create_file.py
+++ supports/integration-test/pyarrow_create_file.py
@@ -5,16 +5,18 @@ import time
 
 from util import *
 
-DIR_NAME = "pyarrow/"
+DIR_NAME = TEST_DIR + random_string() + '/'
 
-def create_file_pyarrow(ip,port,start,end):
-    fs = pa.hdfs.connect(ip,port)
+
+def create_file_pyarrow(ip, port, start, end):
+    fs = pa.hdfs.connect(ip, port)
     fs.mkdir(TEST_DIR+DIR_NAME)
-    for i in range(start,end):
-        with fs.open(TEST_DIR+DIR_NAME+str(i),"wb") as f:
+    for i in range(start, end):
+        with fs.open(DIR_NAME+str(i), "wb") as f:
             f.write("")
 
-def main(ip,port,num):
+
+def main(ip, port, num):
     stime = time.time()
     p = Pool()
     max_task = cpu_count()
@@ -22,16 +24,16 @@ def main(ip,port,num):
     for i in range(max_task):
         start = i*length
         end = start+length
-        if i==max_task-1:
+        if i == max_task-1:
             end = num
-        p.apply_async(create_file_pyarrow,args=(ip,port,start,end,))
+        p.apply_async(create_file_pyarrow, args=(ip, port, start, end,))
     p.close()
     p.join()
     etime = time.time()
-    fs = pa.hdfs.connect(ip,port)
-    print "%fs create %d files" % (etime-stime,len(fs.ls(TEST_DIR+DIR_NAME)))
+    fs = pa.hdfs.connect(ip, port)
+    print "%fs create %d files" % (etime-stime, len(fs.ls(DIR_NAME)))
 
-if __name__=='__main__':
+if __name__ == '__main__':
     ip = "localhost"
     port = 9000
     num = 10000
@@ -42,4 +44,4 @@ if __name__=='__main__':
     except:
         print "Usage: python pyarrow_create_file.py [ip] [port] [num]"
         sys.exit()
-    main(ip,port,num)
+    main(ip, port, num)
