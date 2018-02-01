e1b1b25087956b02e74e2b0628d3c2c0d95c1c87
Polaris Feng <fztfztfztfzt@gmail.com>
2018-02-01 15:17:08 +0800

    create test file use pyarrow

---
+++ supports/integration-test/pyarrow_create_file.py
@@ -0,0 +1,45 @@
+import sys
+from multiprocessing import Pool
+import pyarrow as pa
+import time
+
+from util import *
+
+DIR_NAME = "pyarrow/"
+
+def create_file_pyarrow(ip,port,start,end):
+    fs = pa.hdfs.connect(ip,port)
+    fs.mkdir(TEST_DIR+DIR_NAME)
+    for i in range(start,end):
+        with fs.open(TEST_DIR+DIR_NAME+str(i),"wb") as f:
+            f.write("")
+
+def main(ip,port,num):
+    stime = time.time()
+    p = Pool()
+    max_task = cpu_count()
+    length = num/max_task
+    for i in range(max_task):
+        start = i*length
+        end = start+length
+        if i==max_task-1:
+            end = num
+        p.apply_async(create_file_pyarrow,args=(ip,port,start,end,))
+    p.close()
+    p.join()
+    etime = time.time()
+    fs = pa.hdfs.connect(ip,port)
+    print "%fs create %d files" % (etime-stime,len(fs.ls(TEST_DIR+DIR_NAME)))
+
+if __name__=='__main__':
+    ip = "localhost"
+    port = 9000
+    num = 10000
+    try:
+        ip = sys.argv[1]
+        port = int(sys.argv[2])
+        num = int(sys.argv[3])
+    except:
+        print "Usage: python pyarrow_create_file.py [ip] [port] [num]"
+        sys.exit()
+    main(ip,port,num)
