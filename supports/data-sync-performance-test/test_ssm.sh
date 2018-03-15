c17d78a9f26db5cb9cf1186420f87084155c4cb9
Polaris Feng <fztfztfztfzt@gmail.com>
2018-03-15 13:33:44 +0800

    miscellaneous

---
+++ supports/data-sync-performance-test/test_ssm.sh
@@ -0,0 +1,11 @@
+for i in {1..5}
+do
+for name in 1MB_10000 100MB_1000
+do
+    sh /root/polaris/prepare.sh
+    echo "====================file:$name  time:$i============================"
+    echo "python /root/polaris/IT/ssm.py $name $i" > cmd.sh
+    ./pat run "$name-$i"
+    cp /root/smart-data-1.3.2/logs/smartserver.log "results/$name-$i.log"
+done
+done
