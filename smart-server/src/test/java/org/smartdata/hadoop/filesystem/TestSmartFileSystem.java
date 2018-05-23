9b27d9a44576f98476d48132a159461d0ac620c9
gnillor <frank.zeng@intel.com>
2018-05-23 09:57:56 +0800

    Fix checkstyle error.

--- smart-server/src/test/java/org/smartdata/hadoop/filesystem/TestSmartFileSystem.java
+++ smart-server/src/test/java/org/smartdata/hadoop/filesystem/TestSmartFileSystem.java
@@ -21,7 +21,6 @@ import com.google.gson.Gson;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.DFSUtil;
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
