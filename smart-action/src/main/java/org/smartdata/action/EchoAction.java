eee551232b37ba14ce53284ee398d865ff093b5a
rum2mojito <wead3105@sjtu.edu.cn>
2018-06-06 14:05:13 +0800

    #1770

---
+++ smart-action/src/main/java/org/smartdata/action/EchoAction.java
@@ -0,0 +1,18 @@
+package org.smartdata.action;
+
+import org.smartdata.action.annotation.ActionSignature;
+
+@ActionSignature(
+        actionId = "echo",
+        displayName = "echo",
+        usage = EchoAction.PRINT_MESSAGE + " $message"
+)
+public class EchoAction extends SmartAction {
+    public static final String PRINT_MESSAGE = "-print_message";
+
+    @Override
+    protected void execute() throws Exception {
+        this.appendResult(getArguments().get(PRINT_MESSAGE));
+        System.out.println(getArguments().get(PRINT_MESSAGE));
+    }
+}
