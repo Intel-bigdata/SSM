1b9fa159f5309d29908467a22e15832f017c7be0
xyh771634 <771645135@qq.com>
2018-07-03 07:55:27 -0700

    modify ssm

--- smart-common/src/main/java/org/smartdata/versioninfo/VersionInfoWrite.java
+++ smart-common/src/main/java/org/smartdata/versioninfo/VersionInfoWrite.java
@@ -1,6 +1,12 @@
 package org.smartdata.versioninfo;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
