e468bf9389ba05e22e990840c96c77c8a578a58f
philo <feilong.he@intel.com>
2018-07-04 11:49:28 +0800

    stop rule by check cmdlets

--- supports/data-sync-performance-test/run_ssm_sync.py
+++ supports/data-sync-performance-test/run_ssm_sync.py
@@ -2,22 +2,26 @@ import sys
 import time
 from util import *
 
-case = sys.argv[1]
-round = sys.argv[2]
-dest_cluster = sys.argv[3]
+size = sys.argv[1]
+num = sys.argv[2]
+case = size + "_" + num
+log = sys.argv[3]
+dest_cluster = sys.argv[4]
 
 rid = submit_rule("file: path matches \"/" + case + "/*\" | sync -dest " + dest_cluster + "/" + case)
 start_rule(rid)
-
-start = False
+start_time = time.time()
 while True:
-    runningProgress = get_sync_info_by_rid(rid)["runningProgress"]
-    if start:
-        if runningProgress == 0:
-            break
-    else:
-        if runningProgress > 0:
-            start = True
+    rule = get_rule(rid)
+    if rule['numCmdsGen'] == int(num):
+        break
     time.sleep(1)
-wait_for_cmdlets(get_cids_of_rule(rid))
+failed_cids = wait_for_cmdlets(get_cids_of_rule(rid))
+if len(failed_cids) != 0:
+    print "Not all sync actions succeed!"
 stop_rule(rid)
+end_time = time.time()
+# append result to log file
+f = open(log, 'a')
+f.write(str(int(end_time - start_time)) + "s" + "  ")
+f.close()
--- supports/data-sync-performance-test/test_ssm_sync_performance.sh
+++ supports/data-sync-performance-test/test_ssm_sync_performance.sh
@@ -4,28 +4,37 @@ unset http_proxy
 # for python use
 export PYTHONPATH=../integration-test:$PYTHONPATH
 
+echo "Get configuration from config."
 . config
 echo "------------------ Your configuration ------------------"
 echo "SSM home is ${SMART_HOME}."
 echo "PAT home is ${PAT_HOME}."
-echo "Test cases are ${cases}."
-echo "Dest cluster is ${DEST_CLUSTER}."
+echo "Test cases:"
+for size in ${!cases[@]}; do
+  echo ${size} ${cases[$size]}
+done
+echo "Destination cluster is ${DEST_CLUSTER}."
 echo "--------------------------------------------------------"
 
 bin=$(dirname "${BASH_SOURCE-$0}")
 bin=$(cd "${bin}">/dev/null; pwd)
+log="${bin}/ssm.log"
+# remove historical data in log file
+printf "" > ${log}
 
-# make ssm log empty before test
-echo "" > ${SMART_HOME}/logs/smartserver.log
-
-for case in ${cases}; do
+for size in "${!cases[@]}"; do
+    case=${size}_${cases[$size]}
+    echo "Test case ${case}:" >> ${log}
     for i in {1..5}; do
-        sh prepare.sh ${case}
         echo "==================== test case: $case, test round: $i ============================"
-        pushd ${PAT_HOME}/PAT-collecting-data
-        echo "export PYTHONPATH=${bin}/../integration-test:${PYTHONPATH}; python ${bin}/run_ssm_sync.py ${case} ${i} ${DEST_CLUSTER}" > cmd.sh
-        ./pat run "$case-$i"
+        # make ssm log empty before test
+        printf "" > ${SMART_HOME}/logs/smartserver.log
+        sh prepare.sh ${case}
+        cd ${PAT_HOME}/PAT-collecting-data
+        echo "export PYTHONPATH=${bin}/../integration-test:${PYTHONPATH}; python ${bin}/run_ssm_sync.py ${size} ${cases[$size]} ${log} ${DEST_CLUSTER}" > cmd.sh
+        ./pat run "${case}-$i"
         cp ${SMART_HOME}/logs/smartserver.log ./results/${case}-${i}.log
-        popd
+        cd ${bin}
     done
+    printf "\nTest case ${case} is finished!\n" >> ${log}
 done
