8dcc1e15492ca47f2f05b6e3f0540fa77fe2bce9
yaohuan771634 <huan.yao@intel.com>
2018-07-05 15:24:26 +0800

    test

--- smart-common/src/main/java/org/smartdata/versioninfo/VersionInfoWrite.java
+++ smart-common/src/main/java/org/smartdata/versioninfo/VersionInfoWrite.java
@@ -1,3 +1,22 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
 package org.smartdata.versioninfo;
 
 import java.io.BufferedReader;
@@ -17,16 +36,17 @@ import java.util.TimeZone;
 public class VersionInfoWrite {
   File directory = new File("");
   String pom = directory.getAbsolutePath() + "/" + "pom.xml";
-  String pwd = directory.getAbsolutePath();
 
   public void execute() {
     try {
       File f = new File(directory.getAbsolutePath()
-        + "/" + "common-version-info.properties");
+        + "/smart-common/src/main/resource/" + "versionInfo.properties");
       FileWriter out = new FileWriter(f);
-      out.write("SSM " + getVersionInfo(pom) + "\n");
-      out.write("Subversion " + getSCMUri() + " -r " + getSCMCommit() + "\n");
-      out.write("Compiled by " + getSCMUser() + " on " + getBuildTime() + "\n");
+      out.write("version=" + getVersionInfo(pom) + "\n");
+      out.write("revision=" + getSCMCommit() + "\n");
+      out.write("user=" + getSCMUser() + "\n");
+      out.write("date=" + getBuildTime() + "\n");
+      out.write("url=" + getSCMUri() + "\n");
       out.flush();
       out.close();
     } catch (IOException e) {
