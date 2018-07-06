9219dbfd909aca473cb9ade001473989637f7dd3
yaohuan771634 <huan.yao@intel.com>
2018-07-06 17:21:31 +0800

    finished

--- smart-common/src/main/java/org/smartdata/versioninfo/VersionInfoRead.java
+++ smart-common/src/main/java/org/smartdata/versioninfo/VersionInfoRead.java
@@ -1,70 +1,75 @@
 /**
- *  Licensed to the Apache Software Foundation (ASF) under one
- *  or more contributor license agreements.  See the NOTICE file
- *  distributed with this work for additional information
- *  regarding copyright ownership.  The ASF licenses this file
- *  to you under the Apache License, Version 2.0 (the
- *  "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.smartdata.versioninfo;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.FileReader;
-import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class VersionInfoRead {
-  public void printInfo() throws IOException {
-    String s = this.getClass().getResource("/").getPath()
-            + "/../../src/main/resources/versionInfo.properties";
-    FileReader v = new FileReader(s);
-    BufferedReader br = new BufferedReader(v);
-    String version = "Not found";
-    String url = "Not found";
-    String revision = "Not found";
-    String time = "Not found";
-    String user = "Not found";
-    String st = null;
-    while ((st = br.readLine()) != null) {
-      if (st.contains("user")) {
-        user = st.substring("user=".length());
-      }
-      if (st.contains("date")) {
-        time = st.substring("date=".length());
-      }
-      if (st.contains("revision")) {
-        revision = st.substring("revision=".length());
-      }
-      if (st.contains("url")) {
-        url = st.substring("url=".length());
-      }
-      if (st.contains("version")) {
-        version = st.substring("version=".length());
-      }
+
+  Properties prop = new Properties();
+
+  protected VersionInfoRead(String component) {
+    String s = this.getClass().getResource("/").getPath() + component + "-versionInfo.properties";
+    try {
+      FileReader v = new FileReader(s);
+      InputStream in = new BufferedInputStream(new FileInputStream(s));
+      prop.load(in);
+      in.close();
+    } catch (Exception e) {
+      System.out.println(e);
     }
-    v.close();
-    br.close();
-    System.out.println("SSM " + version);
-    System.out.println("Subversion " + url + " -r " + revision);
-    System.out.println("Compiled by " + user + " on " + time);
+  }
+
+  private static VersionInfoRead info = new VersionInfoRead("common");
+
+  protected String getVersion() {
+    return info.prop.getProperty("version", "Unknown");
+  }
+
+  protected String getRevision() {
+    return info.prop.getProperty("revision", "Unknown");
+  }
+
+  protected String getTime() {
+    return info.prop.getProperty("date", "Unknown");
+  }
+
+  protected String getUser() {
+    return info.prop.getProperty("user", "Unknown");
+  }
+
+  protected String getUrl() {
+    return info.prop.getProperty("url", "Unknown");
+  }
+
+  public void printInfo() {
+    System.out.println("SSM " + getVersion());
+    System.out.println("Subversion " + getUrl() + " -r " + getRevision());
+    System.out.println("Compiled by " + getUser() + " on " + getTime());
   }
 
   public static void main(String[] args) {
-    VersionInfoRead t = new VersionInfoRead();
-    try {
-      t.printInfo();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    VersionInfoRead t = new VersionInfoRead("common");
+    t.printInfo();
   }
 }
--- smart-common/src/main/java/org/smartdata/versioninfo/VersionInfoWrite.java
+++ smart-common/src/main/java/org/smartdata/versioninfo/VersionInfoWrite.java
@@ -1,21 +1,20 @@
 /**
- *  Licensed to the Apache Software Foundation (ASF) under one
- *  or more contributor license agreements.  See the NOTICE file
- *  distributed with this work for additional information
- *  regarding copyright ownership.  The ASF licenses this file
- *  to you under the Apache License, Version 2.0 (the
- *  "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.smartdata.versioninfo;
 
@@ -39,9 +38,9 @@ public class VersionInfoWrite {
 
   public void execute() {
     try {
-      String s = directory.getAbsolutePath()
-        + "/smart-common/src/main/resources/" + "versionInfo.properties";
-      FileWriter out = new FileWriter(s);
+      String s = this.getClass().getResource("/").getPath() + "common-versionInfo.properties";
+      File file = new File(s);
+      FileWriter out = new FileWriter(file);
       out.write("version=" + getVersionInfo(pom) + "\n");
       out.write("revision=" + getSCMCommit() + "\n");
       out.write("user=" + getSCMUser() + "\n");
@@ -59,7 +58,6 @@ public class VersionInfoWrite {
     dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
     return dateFormat.format(new Date());
   }
-
 
   private String getVersionInfo(String pom) throws IOException {
     File fl = new File(pom);
