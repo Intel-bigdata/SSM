23e47629f47a11967c5a71712e565c8314d68cd4
xyh771634 <771645135@qq.com>
2018-07-23 20:35:42 +0800

    ask for con_dir delete ssm home if it exsit firstly

--- bin/install.sh
+++ bin/install.sh
@@ -19,15 +19,45 @@
 BIN_HOME=$(cd `dirname $0`;pwd)
 SSM_HOME=${BIN_HOME%/*}
 SSM_NAME=${SSM_HOME##*/}
+CONF_DIR=$SSM_HOME/conf
 INPUT_PATH=""
 
 cd $SSM_HOME/../;
 
-echo "Do you want to install SSM to the hosts configured in the files 'agents' and 'servers'?"
-echo -e "The hosts are shown below(\033[34mempty means there is no host configured\033[0m):"
+#config option to let user give conf directory, "./install.sh --config" . If not given, use the default conf
+while [ $# != 0 ]; do
+  case "$1" in
+    "--config")
+      shift
+      CONF_DIR_TMP="$1"
+      if [[ ! -d "${CONF_DIR_TMP}" ]]; then
+        echo "ERROR : ${CONF_DIR_TMP} is not a directory"
+        exit 1
+      else
+        CONF_DIR="$1"
+        if [[ $CONF_DIR != */ ]];then
+             CONF_DIR=${CONF_DIR}/
+        fi
+      break
+      fi
+      ;;
+      *)
+      echo no command \"./install.sh $1\"
+      exit 1
+      ;;
+   esac
+done
+
+cat $CONF_DIR/servers $CONF_DIR/agents >>/dev/null 2>/dev/null
+if [ $? = 1 ];then
+ echo -e "maybe there is no 'agents' or 'servers' under $CONF_DIR"
+ exit 1
+fi
+
+echo -e "The configured hosts are shown below(\033[34mempty means there is no host configured\033[0m):"
 
 IFS=$'\n'
-for showhost in `cat $SSM_NAME/conf/servers;echo '';cat $SSM_NAME/conf/agents`
+for showhost in `cat $CONF_DIR/servers;echo '';cat $CONF_DIR/agents`
 do
    showhost=$(echo $showhost | tr -d  " ")
    if [[ "$showhost" =~ ^#.* ]];then
@@ -60,12 +90,17 @@ fi
 
 tar cf "${SSM_NAME}.tar" ${SSM_NAME}
 
-for host in `cat $SSM_NAME/conf/servers;echo '';cat $SSM_NAME/conf/agents`
+for host in `cat $CONF_DIR/servers;echo '';cat $CONF_DIR/agents`
 do
    host=$(echo $host | tr -d  " ")
    if [[ "$host" =~ ^#.* ]];then
         continue
    else
+      #Before install on a host, delete ssm home directory if there exists
+      if_exist=`ssh $host "if [ -d ${INPUT_PATH}${SSM_NAME} ];then echo 1; else echo 0; fi"`
+      if [ $if_exist = 1 ];then
+          ssh $host "rm -rf ${INPUT_PATH}${SSM_NAME}"
+      fi
       flag=`ssh $host "if [ -d $INPUT_PATH ];then echo 1; else echo 0; fi"`
       if [ $flag = 1 ];then
          echo installing SSM to $host...
