7efd08236534adc4437b75f66185b63b15fe6790
rum2mojito <wead3105@sjtu.edu.cn>
2018-08-24 12:38:19 +0800

    fix progress bar

--- smart-zeppelin/zeppelin-web/src/app/dashboard/views/actions/actions.js
+++ smart-zeppelin/zeppelin-web/src/app/dashboard/views/actions/actions.js
@@ -19,7 +19,6 @@
 angular.module('zeppelinWebApp')
 
 
-
   .controller('ActionsCtrl', ActionsCtrl);
   ActionsCtrl.$inject = ['$scope', 'baseUrlSrv', '$filter', '$http', 'conf', '$interval'];
   function ActionsCtrl($scope, baseUrlSrv, $filter, $http, conf, $interval) {
@@ -125,6 +124,66 @@ angular.module('zeppelinWebApp')
       search($scope.path);
     };
 
+    function __search__ (text) {
+      if (!$scope.searching) {
+        $scope.currentSearchPage = 1;
+      }
+      $http.get(baseUrlSrv.getSmartApiRoot() + conf.restapiProtocol + '/actions/search/'
+        + text + '/' + $scope.currentSearchPage + '/' + $scope.pageNumber + '/' + $scope.orderby + '/' + $scope.isDesc)
+        .then(function(response) {
+        var actionData = angular.fromJson(response.data);
+        $scope.totalNumber = actionData.body.totalNumOfActions;
+        $scope.actions = actionData.body.actions;
+        angular.forEach($scope.actions, function (data,index) {
+          data.runTime = data.finishTime - data.createTime;
+          data.createTime = data.createTime === 0 ? "-" :
+            $filter('date')(data.createTime,'yyyy-MM-dd HH:mm:ss');
+          data.finishTime = data.finished ? data.finishTime === 0 ? "-" :
+            $filter('date')(data.finishTime,'yyyy-MM-dd HH:mm:ss') : '-';
+          data.progress = Math.round(data.progress * 100);
+          data.progressColor = data.finished ? data.successful ? 'success' : 'danger' : 'warning';
+        });
+        $scope.totalPage = Math.ceil($scope.totalNumber / $scope.pageNumber);
+      }, function(errorResponse) {
+          $scope.totalNumber = 0;
+      });
+    }
+
+    function search(text) {
+      if (text == "") {
+        $scope.searching = false;
+        getActions();
+      }
+      else {
+        __search__(text);
+        $scope.searching = true;
+      }
+    };
+
+    $scope.getContent = function () {
+      var tmp = document.getElementById('search').value;
+      var res = "";
+      for (var i = 0; i < tmp.length; i++) {
+        if (tmp.charAt(i) == '%') {
+          res += "%25";
+        }
+        else if (tmp.charAt(i) == '/'){
+          res += "%2F";
+        }
+        else if (tmp.charAt(i) == '?'){
+          res += "%3F";
+        }
+        else if (tmp.charAt(i) == '@'){
+          res += "%40";
+        }
+        else {
+          res += tmp.charAt(i);
+        }
+      }
+      $scope.path = res;
+      search($scope.path);
+    };
+
     $scope.gotoPage = function (index) {
       if (!$scope.searching) {
         $scope.currentPage = index;
@@ -139,26 +198,9 @@ angular.module('zeppelinWebApp')
     $scope.defindOrderBy = function (filed) {
       if ($scope.orderby === filed) {
         $scope.isDesc = ! $scope.isDesc;
-        if (filed == 'running_time') {
-          SortByRunnintTime();
-          return;
-        }
-        else if (filed == 'progress') {
-          SortByProgress();
-          return;
-        }
-      }
-      else {
+      } else {
         $scope.orderby = filed;
         $scope.isDesc = true;
-        if (filed == 'running_time') {
-          SortByRunnintTime();
-          return;
-        }
-        else if (filed == 'progress') {
-          SortByProgress();
-          return;
-        }
       }
       if (!$scope.searching) {
         getActions();
@@ -167,53 +209,6 @@ angular.module('zeppelinWebApp')
         __search__($scope.path);
       }
     };
-
-    function SortByRunnintTime() {
-      var count = Object.keys($scope.actions).length;
-      for (var i = 0; i < count && $scope.isDesc; i++) {
-        for (var j = i; j < count ; j++) {
-          if ($scope.actions[i].runTime > $scope.actions[j].runTime) {
-            var tmp = $scope.actions[i];
-            $scope.actions[i] = $scope.actions[j];
-            $scope.actions[j] = tmp;
-          }
-        }
-      }
-
-      for (var i = 0; i < count && !$scope.isDesc; i++) {
-        for (var j = i; j < count ; j++) {
-          if ($scope.actions[i].runTime < $scope.actions[j].runTime) {
-            var tmp = $scope.actions[i];
-            $scope.actions[i] = $scope.actions[j];
-            $scope.actions[j] = tmp;
-          }
-        }
-      }
-    }
-
-
-    function SortByProgress() {
-      var count = Object.keys($scope.actions).length;
-      for (var i = 0; i < count&& $scope.isDesc; i++) {
-        for (var j = i; j < count ; j++) {
-          if ($scope.actions[i].progress > $scope.actions[j].progress) {
-            var tmp = $scope.actions[i];
-            $scope.actions[i] = $scope.actions[j];
-            $scope.actions[j] = tmp;
-          }
-        }
-      }
-
-      for (var i = 0; i < count&& !$scope.isDesc; i++) {
-        for (var j = i; j < count ; j++) {
-          if ($scope.actions[i].progress < $scope.actions[j].progress) {
-            var tmp = $scope.actions[i];
-            $scope.actions[i] = $scope.actions[j];
-            $scope.actions[j] = tmp;
-          }
-        }
-      }
-    }
 
     // getActions();
     if ($scope.totalNumber == 0) {
@@ -238,13 +233,11 @@ angular.module('zeppelinWebApp')
     });
 
     var timer = $interval(function(){
-      if ($scope.orderby != 'running_time' && $scope.orderby != 'progress') {
-        if (!$scope.searching) {
-          getActions();
-        }
-        else {
-          __search__($scope.path);
-        }
+      if (!$scope.searching) {
+        getActions();
+      }
+      else {
+        __search__($scope.path);
       }
     },60000);
 
