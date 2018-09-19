bedb39db139e7b0e55a6facd0575ae5da01271ff
rum2mojito <wead3105@sjtu.edu.cn>
2018-09-19 18:03:14 +0800

    ec test script

---
+++ supports/integration-test/test_ec_action.py
@@ -0,0 +1,35 @@
+import argparse
+import unittest
+from util import *
+
+POLICY = 'XOR-2-1-1024k'
+class test_ec_action(unittest.TestCase):
+
+  # test XOR-2-1-1024k
+  def test_ec(self):
+    file_path = create_random_file(FILE_SIZE)
+    print "The file path for test is {}.".format(file_path)
+    # submit action
+    action_str = "ec -file {} -policy {}".format(file_path, POLICY)
+    # Activate actions
+    cid = submit_cmdlet(action_str)
+    cmd = wait_for_cmdlet(cid)
+    self.assertTrue(cmd['state'] == "DONE", "Test failed for ec action with XOR-2-1-1024k policy.")
+
+    # submit action
+    action_str = "unec -file {}".format(file_path)
+    # Activate actions
+    cid = submit_cmdlet(action_str)
+    cmd = wait_for_cmdlet(cid)
+    self.assertTrue(cmd['state'] == "DONE", "Test failed for unec action.")
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-size', default='1MB')
+  parser.add_argument('unittest_args', nargs='*')
+  args, unknown_args = parser.parse_known_args()
+  sys.argv[1:] = unknown_args
+  print "The file size for test is {}.".format(args.size)
+  FILE_SIZE = convert_to_byte(args.size)
+
+  unittest.main()
