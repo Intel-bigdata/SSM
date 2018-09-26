bdf101e300ded69b1945bef68ed1bb4a37b0d9c8
philo <feilong.he@intel.com>
2018-09-26 14:41:28 +0800

    refine test scripts

--- supports/integration-test/test_ec_rule.py
+++ supports/integration-test/test_ec_rule.py
@@ -2,14 +2,20 @@ import argparse
 import unittest
 from util import *
 
-POLICY = 'XOR-2-1-1024k'
+
 class test_ec_action(unittest.TestCase):
 
   def test_ec_rule(self):
     file_dir = TEST_DIR + random_string() + "/"
     print "The file path for test is {}.".format(file_dir)
+    cids = []
     for i in range(MAX_NUMBER):
-      create_file(file_dir + random_string(), FILE_SIZE)
+      path, cid = create_random_file_parallel(FILE_SIZE, file_dir)
+      cids.append(cid)
+    failed_cids = wait_for_cmdlets(cids)
+    self.assertTrue(len(failed_cids) == 0, "Failed to create test files!")
+    # wait for consistency
+    time.sleep(3)
     # submit rule
     rule_str = "file : path matches \"" + file_dir + '*' + "\" | ec -policy " + POLICY
     rid = submit_rule(rule_str)
@@ -17,45 +23,57 @@ class test_ec_action(unittest.TestCase):
     start_rule(rid)
     # Status check
     rule = get_rule(rid)
-    cmds = get_cmdlets_of_rule(rid)
-    flag = True
-    for cmd in cmds:
-      if cmd['state'] != 'DONE':
-        flag = False
-        break
-    self.assertTrue(flag, "Test failed for ec rule.")
+    while rule['numCmdsGen'] < MAX_NUMBER:
+      rule = get_rule(rid)
+    cids = get_cids_of_rule(rid)
+    failed_cids = wait_for_cmdlets(cids)
+    self.assertTrue(len(failed_cids) == 0, "Test failed for EC rule!")
+    # wait for consistency
+    time.sleep(1)
     stop_rule(rid)
 
   def test_unec_rule(self):
     file_dir_path = TEST_DIR + random_string()
     file_dir = file_dir_path + "/"
     print "The file path for test is {}.".format(file_dir)
+    # create a test dir by creating a file under the dir firstly and then deleting it
+    submit_cmdlet("write -file " + file_dir + "tmp.file -length 10")
+    # wait the metastore sync
+    time.sleep(3)
     # submit action
     action_str = "ec -file {} -policy {}".format(file_dir_path, POLICY)
     # Activate actions
     cid = submit_cmdlet(action_str)
     cmd = wait_for_cmdlet(cid)
+    self.assertTrue(cmd['state'] == "DONE", "Failed to make the test dir an EC one.")
+    cids = []
     for i in range(MAX_NUMBER):
-      create_file(file_dir + random_string(), FILE_SIZE)
-
+      path, cid = create_random_file_parallel(FILE_SIZE, file_dir)
+      cids.append(cid)
+    failed_cids = wait_for_cmdlets(cids)
+    self.assertTrue(len(failed_cids) == 0, "Failed to create test files!")
+    # wait for consistency
+    time.sleep(3)
     rule_str = "file : path matches \"" + file_dir + '*' + "\" | unec"
     rid = submit_rule(rule_str)
     # Activate rule
     start_rule(rid)
     # Status check
-    cmds = get_cmdlets_of_rule(rid)
-    flag = True
-    for cmd in cmds:
-      if cmd['state'] != 'DONE':
-        flag = False
-        break
-    self.assertTrue(flag, "Test failed for unec rule.")
+    rule = get_rule(rid)
+    while rule['numCmdsGen'] < MAX_NUMBER:
+      rule = get_rule(rid)
+    cids = get_cids_of_rule(rid)
+    failed_cids = wait_for_cmdlets(cids)
+    self.assertTrue(len(failed_cids) == 0, "Test failed for EC rule!")
+    # wait for consistency
+    time.sleep(1)
     stop_rule(rid)
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('-size', default='1MB')
   parser.add_argument('-num', default='10')
+  parser.add_argument('-policy', default='XOR-2-1-1024k')
   parser.add_argument('unittest_args', nargs='*')
   args, unknown_args = parser.parse_known_args()
   sys.argv[1:] = unknown_args
@@ -63,5 +81,7 @@ if __name__ == '__main__':
   FILE_SIZE = convert_to_byte(args.size)
   print "The file number for test is {}.".format(args.num)
   MAX_NUMBER = int(args.num)
+  print "The EC policy for test is {}.".format(args.policy)
+  POLICY = args.policy
 
   unittest.main()
