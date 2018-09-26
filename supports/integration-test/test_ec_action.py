597d3a97a7b7f750246eb94c998e5ae619243655
PHILO-HE <feilong.he@intel.com>
2018-09-26 14:46:34 +0800

    Refine test scripts for EC (#1948)

--- supports/integration-test/test_ec_action.py
+++ supports/integration-test/test_ec_action.py
@@ -2,7 +2,7 @@ import argparse
 import unittest
 from util import *
 
-POLICY = 'XOR-2-1-1024k'
+
 class test_ec_action(unittest.TestCase):
 
   # test XOR-2-1-1024k
@@ -26,10 +26,13 @@ class test_ec_action(unittest.TestCase):
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('-size', default='1MB')
+  parser.add_argument('-policy', default='XOR-2-1-1024k')
   parser.add_argument('unittest_args', nargs='*')
   args, unknown_args = parser.parse_known_args()
   sys.argv[1:] = unknown_args
   print "The file size for test is {}.".format(args.size)
   FILE_SIZE = convert_to_byte(args.size)
+  print "The EC policy for test is {}.".format(args.policy)
+  POLICY = args.policy
 
   unittest.main()
