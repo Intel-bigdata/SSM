c4d54134bf347616392d7eabec2b5023aacc7f80
philo <feilong.he@intel.com>
2019-11-27 10:33:29 +0800

    Add test for SmartDFSClient reading EC data created by SSM

--- smart-hadoop-support/smart-hadoop-client-3.1/src/test/java/org/smartdata/hdfs/TestSmartDFSClientReadECData.java
+++ smart-hadoop-support/smart-hadoop-client-3.1/src/test/java/org/smartdata/hdfs/TestSmartDFSClientReadECData.java
@@ -24,16 +24,23 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.smartdata.conf.SmartConf;
 import org.smartdata.conf.SmartConfKeys;
+import org.smartdata.hdfs.action.ErasureCodingAction;
+import org.smartdata.hdfs.action.ErasureCodingBase;
+import org.smartdata.hdfs.action.HdfsAction;
 import org.smartdata.hdfs.action.TestErasureCodingMiniCluster;
 import org.smartdata.hdfs.client.SmartDFSClient;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-public class TestReadECDataBySmartDFSClient extends TestErasureCodingMiniCluster {
+import static org.junit.Assert.assertTrue;
+
+public class TestSmartDFSClientReadECData extends TestErasureCodingMiniCluster {
   public static final String TEST_DIR = "/ec";
 
   @Test
-  public void testReadECData() throws IOException {
+  public void testReadECDataCreatedByHDFS() throws IOException {
 
     cluster.getFileSystem().mkdirs(new Path(TEST_DIR));
     // Set an EC policy for this test dir, so the file created under it will
@@ -50,7 +57,45 @@ public class TestReadECDataBySmartDFSClient extends TestErasureCodingMiniCluster
         SmartConfKeys.SMART_SERVER_RPC_ADDRESS_DEFAULT);
     SmartDFSClient smartDFSClient = new SmartDFSClient(smartConf);
     DFSInputStream dfsInputStream = smartDFSClient.open(srcPath);
-    // In unit test, a DFSInputStream can be still used to read EC data. But in real environment,
+    // In unit test, a DFSInputStream can still be used to read EC data. But in real environment,
+    // DFSStripedInputStream is required, otherwise, block not found exception will occur.
+    Assert.assertTrue(dfsInputStream instanceof DFSStripedInputStream);
+    int bufferSize = 64 * 1024;
+    byte[] buffer = new byte[bufferSize];
+    // Read EC data from HDFS
+    while (dfsInputStream.read(buffer, 0, bufferSize) != -1) {
+    }
+    dfsInputStream.close();
+  }
+
+  @Test
+  public void testReadECDataCreatedBySSM() throws IOException {
+
+    cluster.getFileSystem().mkdirs(new Path(TEST_DIR));
+    String srcPath = "/ec/a.txt";
+    createTestFile(srcPath, 300000);
+    SmartConf smartConf = smartContext.getConf();
+    // The below single configuration is in order to make sure a SmartDFSClient can be created
+    // successfully, and the actual value for this property does't matter.
+    smartConf.set(SmartConfKeys.SMART_SERVER_RPC_ADDRESS_KEY,
+        SmartConfKeys.SMART_SERVER_RPC_ADDRESS_DEFAULT);
+    SmartDFSClient smartDFSClient = new SmartDFSClient(smartConf);
+
+    ErasureCodingAction ecAction = new ErasureCodingAction();
+    ecAction.setContext(smartContext);
+    String ecTmpPath = "/ssm/ec_tmp/tmp_file";
+    Map<String, String> args = new HashMap<>();
+    args.put(HdfsAction.FILE_PATH, srcPath);
+    args.put(ErasureCodingBase.EC_TMP, ecTmpPath);
+    args.put(ErasureCodingAction.EC_POLICY_NAME, ecPolicy.getName());
+    ecAction.init(args);
+    ecAction.run();
+
+    assertTrue(ecAction.getExpectedAfterRun());
+    Assert.assertTrue(ecPolicy == dfsClient.getErasureCodingPolicy(srcPath));
+
+    DFSInputStream dfsInputStream = smartDFSClient.open(srcPath);
+    // In unit test, a DFSInputStream can still be used to read EC data. But in real environment,
     // DFSStripedInputStream is required, otherwise, block not found exception will occur.
     Assert.assertTrue(dfsInputStream instanceof DFSStripedInputStream);
     int bufferSize = 64 * 1024;
